63487c74233af58b23a697efed08749dc04ec3f9
Ascerr <rafalchabros1@gmail.com>
2019-04-12 20:19:44 +0200

    Update Fast SSA.lua

--- Fast SSA.lua
+++ Fast SSA.lua
@@ -1,11 +1,13 @@
 --[[ 
     Script Name: 		Fast Auto SSA
-    Description: 		Spam equipming Stone Skin Amulet (delay 200ms).
+    Description: 		Automatically equip Stone Skin Amulet when slot.amulet is empty. (delay 200ms).
     Author: 			Ascer - example
 ]]
 
 local SSA = 3081	-- id of stone skin amulet
 
 Module.New("Auto SSA", function (mod) -- declare module name and set function mod
-    Self.EquipItem(SLOT_AMULET, SSA, 1, 1) -- equip stone skin amulet to amulet slot with quantity 1.
+    if Self.Amulet().id == 0 then -- get currnet amulet.id
+        Self.EquipItem(SLOT_AMULET, SSA, 1, 1) -- equip stone skin amulet to amulet slot with quantity 1.
+    end        
 end)
