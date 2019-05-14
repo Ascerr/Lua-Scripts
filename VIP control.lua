a7533946c4a155e0947280b126601c53640ba3eb
Ascerr <rafalchabros1@gmail.com>
2019-05-14 09:56:41 +0200

    Update VIP control.lua

--- VIP control.lua
+++ VIP control.lua
@@ -1,20 +1,64 @@
 --[[
     Script Name: 		VIP control
-    Description: 		Check up for buddies from vip list and logout when all are offline.
+    Description: 		Check up for buddies from vip list and logout when all are offline. Edit: 2019-05-14 added relogin.
     Author: 			Ascer - example
 ]]
 
-local LIST = {"mustafa", "rps rps oo"} 	-- list of players from vip list to search.
+local LIST = {"def character", "friend"} 	-- list of players from vip list to search.
+local RELOGIN = {enabled = true, delay = 5} -- relogin to game when player detected or lost connection. enabled = true/false, delay = 6 min or immediately when lost connection
 
 -- DONT'T EDIT BELOW THIS LINE 
 
-list = table.lower(LIST)
+local list, logoutTime, printfTime = table.lower(LIST), 0, 0
+
 
 Module.New("VIP control", function ()
+	
+	-- when we are connected.
 	if Self.isConnected() then
+		
+		-- check if vip is online
 		if not VIP.isOnline(list) then
+			
+			-- logout
 			Self.Logout()
+			
+			-- set message to Rifbot console
 			printf("Logged out due a no players in vip list.")
+
+			-- set time.
+			logoutTime = os.clock()
+
 		end
-	end		
+
+	else
+
+		-- when relogin is enabled
+		if RELOGIN.enabled  then
+        	
+			-- check for time 
+	        if os.clock() - logoutTime > (60 * RELOGIN.delay) then
+
+	        	-- press enter key
+	            Rifbot.PressKey(13, 3000)
+
+	        else
+
+		        -- check for time to avoid spam
+	        	if os.clock() - printfTime > 1 then 
+
+	        		-- set message to console
+			        printf("Successfully logged out due a VIP offline. Relogin for " .. math.floor((RELOGIN.delay * 60) - (os.clock() - logoutTime)) .. "s.")
+		        	
+		        	-- set new time
+		        	printfTime = os.clock()
+
+		        end	
+
+	        end
+
+	    end 
+
+	end	
+
 end)
