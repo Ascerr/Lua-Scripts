dcd2ed2ac0379dd70ffeeadd9848e2c16d8ee583
Ascerr <rafalchabros1@gmail.com>
2019-11-15 10:41:54 +0100

    Update Random UH.lua

--- Random UH.lua
+++ Random UH.lua
@@ -1,19 +1,73 @@
 --[[
-    Script Name: 		Auto Potion
-    Description: 		Restore your character mana
+    Script Name: 		Random UH
+    Description: 		Use RUNE_ID with your character when hp drop below, simulate manual player.
     Author: 			Ascer - example
 ]]
 
-local RUNE_DELAY = {1000, 1500}     -- delay for Pot is default 1000ms we set a little bit higher.
-local MANA_BELOW = 50               -- when your mpperc is below o equal this value character will use POTIONID.
-local POTIONID = 6374                 -- UH id 
+local RUNE_ID = 3160 	-- id of item to use
+local HPPERC_BELOW = 50	-- if hpperc is below this value. (randomized upper 1-5%)
+local RANDOM_SLOW_MOTION = {500, 1000} -- wait this random time when health drop before use uh. (simulate click uh in bp and on yourself time)
 
 -- DONT'T EDIT BELOW THIS LINE 
 
-local mainDelay, mainTime = 0, 0
+-- storage of value.
+randomWait, startTime, hppercBelow = 0, 0, HPPERC_BELOW
 
-Module.New("Auto Potion", function ()
-    if Self.ManaPercent() <= MANA_BELOW then 
-        Self.UseItemWithMe(POTIONID, math.random(RUNE_DELAY[1], RUNE_DELAY[2]))
-    end            
+Module.New("Random UH", function ()
+    
+    -- load self health percent
+    local hpperc = Self.HealthPercent()
+
+    -- when health percent drop below value and startTime is 0, we set config.
+    if hpperc <= hppercBelow and startTime == 0 then
+
+    	-- set random time to wait and startTime.
+    	randomWait = math.random(RANDOM_SLOW_MOTION[1], RANDOM_SLOW_MOTION[2])/1000
+    	startTime = os.clock()
+
+    	printf("set time")
+    	
+    end
+    
+    -- when time is already set (our hp was below value)
+    if startTime ~= 0 then
+
+    	-- when time to use rune already come.
+    	if (os.clock() - startTime) >= randomWait then
+
+    		printf("time is ok")
+
+    		-- check for hpperc. If this is still below value use rune.
+    		if hpperc <= hppercBelow then
+
+    			-- use rune with me to heal from dmg.
+    			Self.UseItemWithMe(RUNE_ID)
+
+    			-- rest values.
+    			randomWait, startTime, hppercBelow = 0, 0, HPPERC_BELOW + math.random(1, 5)
+
+    		else		
+
+    			-- if our hpperc is above value so if it's only above 7% use uh is this case.	
+    			if (hpperc - hppercBelow) <= 7 then
+
+    				-- use rune with me to heal from dmg.
+    				Self.UseItemWithMe(RUNE_ID)
+
+    				-- rest values.
+    				randomWait, startTime, hppercBelow = 0, 0, HPPERC_BELOW + math.random(1, 5)
+
+    			else
+    			
+    				-- Our hpperc look like someone heal us or we do it manually, rest values.
+    				randomWait, startTime, hppercBelow = 0, 0, HPPERC_BELOW + math.random(1, 5)
+
+    			end
+    			
+    		end
+    		
+    	end
+    
+    end
+
 end)
