37bbb2c33a3df8303ea39017a8a0144b69ddaf72
Ascerr <rafalchabros1@gmail.com>
2019-12-30 19:30:36 +0100

    Add files via upload

---
+++ Pickup MF around you.lua
@@ -0,0 +1,30 @@
+--[[
+    Script Name:        Pickup MF around you
+    Description:        Pickup mana fluid around your character to container.index 0 slot 0
+    Author:             Ascer - example
+]]
+
+local MF_ID = 2874     -- set 2006 for old tibia
+
+-- DON'T EDIT BELOW
+
+Module.New("Pickup MF around you", function (mod)
+
+	if Self.isConnected() then
+		
+		local map = Map.getArea(1) -- load map with 1 sqm range
+		for i, square in pairs(map) do
+			local sqareItems = square.items
+			for j, item in pairs(sqareItems) do
+				if item.id == MF_ID then
+					
+					-- Pickup item
+					Self.PickupItem(square.x, square.y, square.z, item.id, 1, 0, 0, 300)
+
+					break
+	
+				end
+			end			
+		end
+	end		
+end) 
