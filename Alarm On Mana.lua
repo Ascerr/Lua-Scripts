28ecc728f059f89ab3e03a40b9f1a459311df268
Ascerr <rafalchabros1@gmail.com>
2020-01-10 15:59:40 +0100

    Add files via upload

---
+++ Alarm On Mana.lua
@@ -0,0 +1,14 @@
+--[[
+    Script Name: 		Alarm On Mana
+    Description: 		Play sound if your health is below 50%.
+    Author: 			Ascer - example
+]]
+
+local MANA = 1000				-- when mana will equal or above this value play sound.
+local ALARM_NAME = "Low Mana.mp3" -- find more in Rifbot Lib.lua
+
+Module.New("Alarm On Mana", function ()
+	if Self.Mana() >= MANA then
+		Rifbot.PlaySound(ALARM_NAME)
+	end	
+end)
