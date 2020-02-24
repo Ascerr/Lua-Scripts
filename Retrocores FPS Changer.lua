c7f6dad46a26b45b90887bd5b44a4919ebffac82
Ascerr <rafalchabros1@gmail.com>
2020-02-24 08:25:09 +0100

    Add files via upload

---
+++ Retrocores FPS Changer.lua
@@ -0,0 +1,19 @@
+--[[
+    Script Name: 		Retrocores FPS Changer.
+    Description: 		Change fps ingame to lower only on Retrocores Server.
+    Author: 			Ascer - example
+]]
+
+local FPS = 25 -- amount of fps to set
+local FPS_ADDRESS = 0x525FF4 -- memory address with fps value to set (edit only when game update if you've knowladge)
+
+-- DON'T EDIT BELOW THIS LINE
+
+local addrBase = Rifbot.getClientInfo().base
+
+Module.New("Realesta FPS Changer", function()
+    if Rifbot.MemoryRead(addrBase + FPS_ADDRESS) ~= FPS then
+        Rifbot.MemoryWrite(addrBase + FPS_ADDRESS, FPS)
+    end    
+    mod:Delay(2000)
+end)
