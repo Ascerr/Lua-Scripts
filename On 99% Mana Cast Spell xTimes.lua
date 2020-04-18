e014139a97589a3139ee934bd82de4a72bc9e738
Ascerr <rafalchabros1@gmail.com>
2020-04-18 17:40:49 +0200

    Update On 99% Mana Cast Spell xTimes.lua

--- On 99% Mana Cast Spell xTimes.lua
+++ On 99% Mana Cast Spell xTimes.lua
@@ -4,12 +4,44 @@
     Author:             Ascer - example
 ]]
 
-local MANA_PERCENT = 99		-- cast spell when mana percent reach this %.
-local CAST_TIMES = 4		-- cast spell x times.
-local SPELL = "exura"		-- spell name
+local MANA_PERCENT = 99					-- cast spell when mana percent reach this %.
+local CAST_TIMES = 4					-- cast spell x times.
+local SPELL = "exura"					-- spell name
+local WHEN_PALYER_DONT_DO_THIS = false	-- when player appear on screen don't cast spell
+local FRIENDS = {"Friend1", "Friend2"}  -- ignore this names (Capital letter)
 
 -- DON'T EDIT BELOW THIS LINE.
 
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+--> Function:       getPlayer()
+--> Description:    Get player on screen
+--> Class:          Self
+--> Params:         None              
+--> Return:         boolean true or false
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+function getPlayer()
+
+    -- inside loop for all found creatures on multiple floors do:
+    for i, c in pairs(Creature.iPlayers(7, false)) do
+
+        -- when we can not find a friends.
+        if not table.find(FRIENDS, player.name) then
+            
+            -- return creature.    
+            return true
+
+        end        
+        
+    end
+
+    -- return false noone player found.
+    return false
+
+end 
+
+-- Module working in loop.
 Module.New("On 99% Mana Cast Spell xTimes", function (mod)
 	
 	-- when we are connected.
@@ -20,6 +52,14 @@ Module.New("On 99% Mana Cast Spell xTimes", function (mod)
 
 			-- inside loop cast spell.
 			for i = 1, CAST_TIMES do
+
+				-- when we check for players
+				if WHEN_PALYER_DONT_DO_THIS then
+				
+					-- when player detected break
+					if getPlayer() then break end
+
+				end	
 
 				-- cast spell.
 				Self.CastSpell(SPELL)
