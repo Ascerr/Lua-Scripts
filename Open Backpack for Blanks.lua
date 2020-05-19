19f34299f6fcce6043ab5ce8ea1822301e221e75
Ascerr <rafalchabros1@gmail.com>
2020-05-19 15:46:20 +0200

    Update Open Backpack for Blanks.lua

--- Open Backpack for Blanks.lua
+++ Open Backpack for Blanks.lua
@@ -7,17 +7,23 @@
 ]]
 
 local BLANK_RUNE_ID = 2260		-- id of blank rune // new id: 3147
-local BACKPACK_ID = 2004		-- id of backpack with blank runes
+local BACKPACK_ID = 1988		-- id of backpack with blank runes
+local SLOT = SLOT_WEAPON		-- where you put blank runes SLOT_WEAPON or SLOT_SHIELD
 
 -- DON'T EDIT BELOW
 
 Module.New("Open Backpack for Blanks", function(mod)
 	local blank = Container.FindItem(BLANK_RUNE_ID)
-	if not blank then
-		local newCont = Container.FindItem(BACKPACK_ID) 
-		if table.count(newCont) > 1 then
-			Container.UseItem(newCont.index, newCont.slot, newCont.id, false) -- open new container in this same index
-		end	
-	end	
+	local hand = selfGetEquipmentSlot(SLOT)
+	if hand.id ~= 0 and hand.id ~= BLANK_RUNE_ID then
+		Self.DequipItem(SLOT)
+	else
+		if not blank then
+			local newCont = Container.FindItem(BACKPACK_ID) 
+			if table.count(newCont) > 1 then
+				Container.UseItem(newCont.index, newCont.slot, newCont.id, false) -- open new container in this same index
+			end	
+		end
+	end		
 	mod:Delay(700, 1200)
 end)
