d823b6d925f39bfb7839734bef6ada320b822b2f
Ascerr <rafalchabros1@gmail.com>
2020-07-20 18:46:22 +0200

    Add files via upload

---
+++ Mouse Macro.lua
@@ -0,0 +1,14 @@
+--[[
+    Script Name:        Mouse Macro
+    Description:        Click right on rune and use it on your character.
+    Author:             Ascer - example
+]]
+
+
+
+-- right click
+Rifbot.MouseClick(1190, 504, 1)
+
+-- left click
+Rifbot.MouseClick(584, 335, 0)
+
