d94bc2540c24733973226bb1bfe084353ca433f3
Ascerr <rafalchabros1@gmail.com>
2020-08-01 19:40:08 +0200

    Add files via upload

---
+++ Stop Bot on GM.lua
@@ -0,0 +1,18 @@
+--[[
+    Script Name: 		Stop Bot on GM
+    Description: 		Stop bot when GM without invisible mode detected.
+    Author: 			Ascer - example
+]]
+
+
+-- DONT'T EDIT BELOW THIS LINE 
+
+Module.New("Stop Bot on GM", function ()
+	local players = Creature.iPlayers(7, false) -- get players on screen only
+    for i = 1, #players do
+        local player = players[i]
+        if string.instr(player.name, "GM ") or string.instr(player.name, "CM ") or string.instr(player.name, "Admin ") or string.instr(player.name, "ADM ") then
+            return Rifbot.setEnabled(false)
+        end
+    end
+end)
