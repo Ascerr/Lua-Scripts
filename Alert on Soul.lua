3e75289c7cf5ae2ef44254a7a8cc7b8369ddeae4
Ascerr <rafalchabros1@gmail.com>
2020-12-08 10:36:27 +0100

    Add files via upload

---
+++ Alert on Soul.lua
@@ -0,0 +1,19 @@
+--[[
+    Script Name:        Alert on Soul
+    Description:        Play sound when low soul points
+    Author:             Ascer - example
+]]
+
+local SOUL = 50		-- whe your soul points will equal or below this amount play sound.
+
+--  DON'T EDIT BELOW THIS LINE
+
+Module.New("Alert on Soul", function (mod)
+	if Self.isConnected() then
+		if Self.Soul() <= SOUL then
+			Rifbot.PlaySound("Default.mp3")
+			printf("Alert: Low soul points")
+		end
+	end			
+	mod:Delay(300, 1200) -- set random delay
+end)
