ee17509f5c354f564d6c13a8102b1b6ef207c7e8
Ascerr <rafalchabros1@gmail.com>
2021-02-02 17:28:58 +0100

    Update Fast UH Healing.lua

--- Fast UH Healing.lua
+++ Fast UH Healing.lua
@@ -7,11 +7,24 @@
 local RUNE_DELAY = 1000				-- time between using rune. Default min delay on use uh is 1000ms
 local HPPERC_BELOW = 50             --when your hpperc is below or equal this value
 local RUNEID = 3160                 -- UH id 
+local USE_ONLY_IF_MANA_BELOW = {enabled = false, mana = 160}  -- use uh only if you have low mana. @enabled - true/false, @mana - use if mana below this value
 
 -- DONT'T EDIT BELOW THIS LINE 
 
+-- module to run in loop 200ms
 Module.New("Fast UH Healing", function ()
-    if Self.HealthPercent() <= HPPERC_BELOW then 
-        Self.UseItemWithMe(RUNEID, RUNE_DELAY)
-    end            
+    
+	-- when connected
+    if Self.isConnected() then
+		
+		-- check if mana enabled
+		local var = (USE_ONLY_IF_MANA_BELOW.enabled and Self.Mana() <= USE_ONLY_IF_MANA_BELOW.mana) or not USE_ONLY_IF_MANA_BELOW.enabled
+	    
+		-- check for hpperc.
+	    if Self.HealthPercent() <= HPPERC_BELOW and var then 
+	        Self.UseItemWithMe(RUNEID, RUNE_DELAY)
+	    end
+
+	end 
+
 end)
