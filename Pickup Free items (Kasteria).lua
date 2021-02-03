9477ed77a14aa564cc109aa408d7d85790d26a70
Ascerr <rafalchabros1@gmail.com>
2021-02-03 13:51:26 +0100

    Add files via upload

---
+++ Pickup Free items (Kasteria).lua
@@ -0,0 +1,42 @@
+--[[
+    Script Name:        Pickup Free items
+    Description:        Pickup free items around your character to first slot in any opened backpack.
+    Author:             Ascer - example
+]]
+
+local FREE_ITEMS = {2874, 3031}     -- IDs of items to pickup
+
+-- DON'T EDIT BELOW
+
+Module.New("Pickup Free items", function ()
+
+	if Self.isConnected() then
+		
+		-- load self pos.
+		local pos = Self.Position()
+
+		-- in range for 1 sqm
+		for x = -1, 1 do
+
+			for y = -1 , 1 do
+				
+				-- load map
+				local map = Map.GetTopMoveItem(pos.x + x, pos.y + y, pos.z)
+
+				-- when we found item
+				if table.find(FREE_ITEMS, map.id) then
+
+					-- Pickup item
+					Self.PickupItem(pos.x + x, pos.y + y, pos.z, map.id, map.count, Container.GetWithEmptySlots(nr), 0)
+
+					-- break loop
+					break
+
+				end	
+
+			end
+			
+		end		
+	end
+
+end) 
