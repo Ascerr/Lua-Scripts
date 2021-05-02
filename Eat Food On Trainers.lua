01bab9e30958aede631a5a3e50357d326f7ccb06
Ascerr <rafalchabros1@gmail.com>
2021-05-02 08:06:32 +0200

    Update Eat Food On Trainers.lua

--- Eat Food On Trainers.lua
+++ Eat Food On Trainers.lua
@@ -4,7 +4,7 @@
     Author:             Ascer - example
 ]]
 
-local MAIN_DELAY = {4, 9}          -- mintues between run antiidle function.
+local MAIN_DELAY = {4, 9}          -- mintues between run function.
 local USE_DELAY = {200, 650}       -- miliseconds between use action.
 local USE_TRIES = {4, 7}           -- amount of times to use .
 local EAT_POS = {x = 0, y = -1, stack = 2}    -- @x, y: positions of food from your character. @stack - index of item on square.
