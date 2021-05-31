b5ad13dd57856ed057a118960c6b3624cb405504
Ascerr <rafalchabros1@gmail.com>
2021-05-31 20:42:16 +0200

    Update Cast Spell On Time.lua

--- Cast Spell On Time.lua
+++ Cast Spell On Time.lua
@@ -4,10 +4,12 @@
     Author:             Ascer - example
 ]]
 
-local SPELL = "utevo lux"           -- spell we will cast ig game
+local SPELL = "utevo lux"           -- spell we will cast in game
 local EVERY_TIME_SECONDS = 80       -- every this time in seconds will countinue cast spell
 local ADD_RANDOM = false            -- add random time +5, 10 seconds [true/false]
-local IF_PLAYER_DONT_CAST = false   -- don't cast if on screen, ignore players from Friends.txt
+local IF_PLAYER_DONT_CAST = true    -- don't cast if on screen, ignore players from Friends.txt
+local WEAR_BLANK_RUNE = false       -- wear blank rune before casting spell
+local BLANK_RUNE_ID = 2260          -- id of blank rune
 
 -- DONT'T EDIT BELOW THIS LINE
 
@@ -42,6 +44,17 @@ Module.New("Cast Spell On Time", function ()
 
             -- when checking
             if not getPlayer() then 
+
+                -- when equiping blanks
+                if WEAR_BLANK_RUNE then
+
+                    -- equip blank rune
+                    Self.EquipItem(SLOT_WEAPON, BLANK_RUNE_ID, 1, 0)
+
+                    -- wait time after equip blank (1000ms = 1s)
+                    wait(3000)
+
+                end 
 
                 -- cast spell
                 Self.CastSpell(SPELL, 0)
