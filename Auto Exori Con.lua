bcbc6590f16e28d3399cb3e50d8fb9af3f4a7ae3
Ascerr <rafalchabros1@gmail.com>
2021-06-10 17:11:13 +0200

    Update Auto Exori Con.lua

--- Auto Exori Con.lua
+++ Auto Exori Con.lua
@@ -6,12 +6,35 @@
 
 local SPELL = "exori con"   -- spell to cast
 local HPPERC = 50			-- don't cast if hp below %
+local MONSTERS = {"Dragon", "Dragon Lord", "Rat"} -- shoot only with in this monsters.
 
+-- DON'T EDIT BELOW
+MONSTERS = table.lower(MONSTERS)
+
+-- module
 Module.New("Auto Exori Con", function (mod)
+    
+	-- load self hpperc and targetid
     local hp = Self.HealthPercent()
     local target = Self.TargetID()
+
+    -- when hp is ok and we have target
     if hp > HPPERC and target > 0 then
-        Self.CastSpell(SPELL)
+
+    	-- load attacked creature
+        local mob = Creature.getCreatures(target)
+        
+        -- when name of this creature is in table.
+        if table.find(MONSTERS, string.lower(mob.name)) then
+
+        	-- cast spell
+        	Self.CastSpell(SPELL)
+
+        end
+        	
     end
-    mod:Delay(300, 500)    
+
+    -- execure delay
+    mod:Delay(300, 500)
+
 end)
