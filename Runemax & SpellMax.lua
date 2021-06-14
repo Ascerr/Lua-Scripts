242865222a6b8443b9fb07cce59d87f115a6055a
Ascerr <rafalchabros1@gmail.com>
2021-06-14 07:51:35 +0200

    Update Runemax & SpellMax.lua

--- Runemax & SpellMax.lua
+++ Runemax & SpellMax.lua
@@ -6,56 +6,69 @@
 
 local CONFIG = {
     rune = {id = 3155, range = 7, enabled = true},              -- shoot rune with target, [id] of rune (3155 is SD), [range] - sqms to target, [enabled] - true/false
-    spell = {name = "exori con", range = 7, enabled = false}    -- cast spell with target, [name] of spell (exori con), ..
+    spell = {name = "exori con", range = 7, enabled = false},   -- cast spell with target, [name] of spell (exori con), ..
+    health = 60,                                                -- dont shoot/cast if self character hpperc below this value
+    monsters = {"Rat", "Snake"}                                 -- monsters names to cast spell or shoot rune.    
 }
 
 -- DON'T EDIT BELOW THIS LINE
 
+-- converting table to lowercase
+CONFIG.monsters = table.lower(CONFIG.monsters)
+
 Module.New("Runemax & SpellMax", function()
 
-    -- load target id.
-    local target = Self.TargetID()
+    -- load self hpperc
+    local hp = Self.HealthPercent()
 
-    -- check for target id.
-    if target > 0 then
+    -- when hp is above value
+    if hp > CONFIG.health then
 
-        -- load creature 
-        local c = Creature.getCreatures(target)
+        -- load target id.
+        local target = Self.TargetID()
 
-        -- when is table.
-        if table.count(c) > 0 then
+        -- check for target id.
+        if target > 0 then
 
-            -- when creature is alive.
-            if c.hpperc > 0 then
+            -- load creature 
+            local c = Creature.getCreatures(target)
 
-                -- check for rune config.
-                if CONFIG.rune.enabled then
+            -- when is table.
+            if table.count(c) > 0 then
 
-                    -- when range is ok.
-                    if Creature.DistanceFromSelf(c) <= CONFIG.rune.range then
+                -- when creature is alive and is valid monster name.
+                if c.hpperc > 0 and table.find(CONFIG.monsters, string.lower(c.name)) then
 
-                        -- shoot rune.
-                        Self.UseItemWithCreature(c, CONFIG.rune.id, math.random(1900, 2200))
+                    -- check for rune config.
+                    if CONFIG.rune.enabled then
+
+                        -- when range is ok.
+                        if Creature.DistanceFromSelf(c) <= CONFIG.rune.range then
+
+                            -- shoot rune.
+                            Self.UseItemWithCreature(c, CONFIG.rune.id, math.random(2000, 2300))
+
+                        end    
+
+                    -- check for spell config    
+                    elseif CONFIG.spell.enabled then
+
+                         -- when range is ok.
+                        if Creature.DistanceFromSelf(c) <= CONFIG.spell.range then
+
+                            -- cast spell.
+                            Self.CastSpell(CONFIG.spell.name, 20, math.random(700, 1500))
+
+                        end    
 
                     end    
 
-                -- check for spell config    
-                elseif CONFIG.spell.enabled then
+                end   
 
-                     -- when range is ok.
-                    if Creature.DistanceFromSelf(c) <= CONFIG.spell.range then
+            end    
 
-                        -- cast spell.
-                        Self.CastSpell(CONFIG.spell.name, 20, math.random(700, 1500))
+        end
 
-                    end    
-
-                end    
-
-            end   
-
-        end    
-
-    end    
+    end        
 
 end)
