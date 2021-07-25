8b8fa97203dd10e5037df5663993bf4beec98492
Ascerr <rafalchabros1@gmail.com>
2021-07-25 19:15:03 +0200

    Update Pickup Free items.lua

--- Pickup Free items.lua
+++ Pickup Free items.lua
@@ -1,10 +1,10 @@
 --[[
     Script Name:        Pickup Free items
-    Description:        Pickup mana fluid around your character to container.index 0 slot 0
+    Description:        Pickup free items around your character to first slot in any opened backpack.
     Author:             Ascer - example
 ]]
 
-local FREE_ITEMS = {2874, 3031}     -- IDs of items to pickup
+local FREE_ITEMS = {2874, 3031, 3447}     -- IDs of items to pickup
 
 -- DON'T EDIT BELOW
 
@@ -12,19 +12,31 @@ Module.New("Pickup Free items", function ()
 
 	if Self.isConnected() then
 		
-		local map = Map.getArea(1) -- load map with 1 sqm range
-		for i, square in pairs(map) do
-			local sqareItems = square.items
-			for j, item in pairs(sqareItems) do
-				if table.find(FREE_ITEMS, item.id) then
-					
-					-- Pickup item
-					Self.PickupItem(square.x, square.y, square.z, item.id, item.count, 0, 0)
+		-- load self pos.
+		local pos = Self.Position()
 
+		-- in range for 1 sqm
+		for x = -1, 1 do
+
+			for y = -1 , 1 do
+				
+				-- load map
+				local map = Map.GetTopMoveItem(pos.x + x, pos.y + y, pos.z)
+
+				-- when we found item
+				if table.find(FREE_ITEMS, map.id) then
+
+					-- Pickup item
+					Self.PickupItem(pos.x + x, pos.y + y, pos.z, map.id, map.count, Container.GetWithEmptySlots(nr), 0, 0)
+
+					-- break loop
 					break
-	
-				end
-			end			
-		end
-	end		
+
+				end	
+
+			end
+			
+		end		
+	end
+
 end) 
