e599b6d32af89a828e0da7f68530c46e7cf1d4e0
Ascerr <rafalchabros1@gmail.com>
2021-07-25 19:02:37 +0200

    Update Fishing.lua

--- Fishing.lua
+++ Fishing.lua
@@ -4,36 +4,33 @@
     Author: 			Ascer - example
 ]]
 
-local MAIN_DELAY = {1500, 2900}  -- miliseconds use fishing rod time
+local MAIN_DELAY = {1000, 1500}  -- miliseconds use fishing rod time
 local FISHING_ROD = 3483		 -- fishing rod id
-local WATER_TILES =   {4597,4598,4599,4600, 4601, 4602, 4609} --{617, 618, 619, 620, 621}  {4597, 4598, 4599, 4600, 4601, 4602} -- ids of water fields
+local WATER_TILES =  {617, 618, 619, 620, 621} --  {4597,4598,4599,4600, 4601, 4602, 4609}  -- ids of water fields
 
 -- DONT'T EDIT BELOW THIS LINE 
 
 local selfloc, fishingPos = Self.Position(), {}
 
-Module.New("fishing", function (mod)
+Module.New("Fishing", function (mod)
 	if Self.isConnected() then
 		local pos = Self.Position()
 		local load = false
 		
-		-- check if postion changed or fishingPos table is empty.
+		-- check if position changed or fishingPos table is empty.
 		if ((pos.x ~= selfloc.x) or (pos.y ~= selfloc.y) or (pos.z ~= selfloc.z)) or table.count(fishingPos) == 0  then 
 			load = true
-			selfloc = Self.Position() -- set last position
+			selfloc = pos -- set last position
 		end
 		
 		if load then -- load a new tiles
 			fishingPos = {} -- reset fishing pos
-			local map = Map.getArea(7) -- load map is CPU eatable function be careful with using it.	
-			for i, square in pairs(map) do
-				if square.amount == 1  then -- check only for tile contains 1 item
-					local items = square.items
-					for j, item in pairs(items) do
-						if table.find(WATER_TILES, item.id) then
-							table.insert(fishingPos, {x = square.x, y = square.y, z = square.z})
-						end
-					end
+			for x = -7, 7 do
+				for y = -5, 5 do
+					local map = Map.GetTopMoveItem(pos.x + x, pos.y + y, pos.z)
+					if table.find(WATER_TILES, map.id) then	
+						table.insert(fishingPos, {x = pos.x + x, y = pos.y + y, z = pos.z})
+					end		
 				end
 			end
 		end
@@ -47,3 +44,5 @@ Module.New("fishing", function (mod)
 	end	
 	mod:Delay(MAIN_DELAY[1], MAIN_DELAY[2]) -- set random delay
 end)
+
+
