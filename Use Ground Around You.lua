e37b795ed60260ef28269ec21346586a6a3f10b6
Ascerr <rafalchabros1@gmail.com>
2021-07-25 19:33:59 +0200

    Update Use Ground Around You.lua

--- Use Ground Around You.lua
+++ Use Ground Around You.lua
@@ -5,7 +5,7 @@
 ]]
 
 local OPEN_DELAY = 200		-- use ground every 200 miliseconds (default)
-local CROPSE_IDS = {3994, 5964}	-- enter here ids of dead monsters to open.
+local CROPSE_IDS = {3994, 5964, 4173}	-- enter here ids of dead monsters to open.
 
 
 -- DONT'T EDIT BELOW THIS LINE
@@ -14,24 +14,6 @@ local lastPos = Self.Position()
 local tablePos = {{-1, -1}, {-1, 0}, {-1, 1}, {1, -1}, {1, 0}, {1, 1}, {0, 1}, {0, -1}} 
 local offset = 1
 
-----------------------------------------------------------------------------------------------------------------------------------------------------------
---> Function:		getMap(x, y, z)
---> Description: 	Read tile on x, y, z for table with items
---> Params:			
--->					@x coordinate in the map on the x-axis
--->					@y coordinate in the map on the y-axis
--->					@z coordinate in the map on the z-axis
---> Return: 		table = {{id=?, count=?}, {id2=?, count2=?}}	
-----------------------------------------------------------------------------------------------------------------------------------------------------------
-function getTile(x, y, z)
-	local map = Map.getArea(1)
-	for i, square in pairs(map) do
-		if square.x == x and square.y == y and square.z == z then 
-			return square.items
-		end	
-	end
-	return -1			
-end	
 
 -- run function in loop here
 Module.New("Use Ground Around You", function (mod)
@@ -48,28 +30,18 @@ Module.New("Use Ground Around You", function (mod)
 	end	
 
 	-- load map
-	local map = getTile(pos.x + tablePos[offset][1], pos.y + tablePos[offset][2], pos.z)	
+	local map = Map.GetTopMoveItem(pos.x + tablePos[offset][1], pos.y + tablePos[offset][2], pos.z)
 
 	-- set last pos
 	lastPos = Self.Position()
 
-	-- when map is different than -1
-	if map ~= -1 then
-		
-		for i, item in pairs(map) do
+	if table.find(CROPSE_IDS, map.id) then
 
-			if table.find(CROPSE_IDS, item.id) then
-
-				-- Use map.
-				Map.UseItem(pos.x + tablePos[offset][1], pos.y + tablePos[offset][2], pos.z, item.id, i-1, 0)
-
-				break
-
-			end	
-		
-		end
+		-- Use map.
+		Map.UseItem(pos.x + tablePos[offset][1], pos.y + tablePos[offset][2], pos.z, map.id, 1, 0)
 
 	end	
+
 
 	-- add offset
 	offset = offset + 1
