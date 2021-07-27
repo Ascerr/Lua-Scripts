a0212b6fd160d087027992c35fae6c06d220b90d
Ascerr <rafalchabros1@gmail.com>
2021-07-27 07:44:00 +0200

    Update Avoid Front Wave.lua

--- Avoid Front Wave.lua
+++ Avoid Front Wave.lua
@@ -1,15 +1,16 @@
 --[[
     Script Name:        Avoid Front Wave
-    Description:        Step to free dir to avoid front wave from monster. [Works only for servers with cavebot]
+    Description:        Step to free dir to avoid front wave from monster. [Works only for Kasteria and Medivia]
     Author:             Ascer - example
 ]]
 
-local STEP_DELAY = 700					-- step every x ms to avoid overdash.
-local ALLOW_WALK_IDS = {123}			-- enter here id such as parcels, boxes etc we check for it.
-local MONSTERS = {"rat", "cave rat", "cyclops"}	-- monsters to active
+local STEP_DELAY = 700										-- step every x ms to avoid overdash.
+local ALLOW_WALK_IDS = {123, 2118, 2119, 2123, 2125, 2124}	-- enter here id such as parcels, boxes, fields etc we check for it.
+local MONSTERS = {"dragon", "dragon lord"}					-- monsters to active
+local TURN_TO_TARGET = true 								-- true/false turn self direction to target
 
 -- DON'T EDIT BELOW THIS LINE
-local stepTime = 0
+local stepTime, modTurnTime, modTurnRand = 0, 0, 0
 
 -- lower case table with monsters names
 MONSTERS = table.lower(MONSTERS)
@@ -59,7 +60,7 @@ end
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------
 --> Function:		delayedStep(dir)
---> Description: 	Get creature you already attack	
+--> Description: 	Step with delay
 --> Params:			
 -->
 -->					@dir number direction to step
@@ -70,7 +71,38 @@ function delayedStep(dir)
 		Self.Step(dir)
 		stepTime = os.clock()
 	end	
-end	
+end
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+--> Function:		getTurnDirToTarget(c)
+--> Description: 	Get possible steps dir near target.
+--> Params:			
+-->
+-->					@c table with creature
+--> Return: 		number dir
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+function getTurnDirToTarget(c)
+	local s = Self.Position()
+	local dir = -1
+	if c.x == (s.x + 1) and c.y == (s.y + 1) then
+		dir = 1
+	elseif c.x == (s.x) and c.y == (s.y + 1) then
+		dir = 2
+	elseif c.x == (s.x - 1) and c.y == (s.y + 1) then
+		dir = 3
+	elseif c.x == (s.x - 1) and c.y == (s.y) then
+		dir = 3
+	elseif c.x == (s.x - 1) and c.y == (s.y - 1) then
+		dir = 0	
+	elseif c.x == (s.x) and c.y == (s.y - 1) then
+		dir = 0
+	elseif c.x == (s.x + 1) and c.y == (s.y - 1) then
+		dir = 1
+	elseif c.x == (s.x + 1) and c.y == (s.y) then
+		dir = 1
+	end
+	return dir	
+end		
 
 -- module run function in loop 200ms
 Module.New("Avoid Front Wave", function()
@@ -91,6 +123,19 @@ Module.New("Avoid Front Wave", function()
 						delayedStep(1)
 					elseif tileIsWalkable(s.x - 1, s.y, s.z) then
 						delayedStep(3)
+					end	
+				end
+				--> turn to target
+				if TURN_TO_TARGET then
+					if Creature.DistanceFromSelf(t) == 1 then
+						if os.clock() - modTurnTime > (modTurnRand / 1000) then
+							local dir = getTurnDirToTarget(t)
+							if dir ~= -1 then
+								Self.Turn(dir)
+								modTurnTime = os.clock()
+								modTurnRand = math.random(300, 800)	
+							end	
+						end	
 					end	
 				end	
 			end	
