c3c19684c75eb7796784c999f54db347ddd02ea1
Ascerr <rafalchabros1@gmail.com>
2021-08-04 12:14:43 +0200

    Update Fishing Through Window.lua

--- Fishing Through Window.lua
+++ Fishing Through Window.lua
@@ -6,7 +6,8 @@
 
 local config = {
 	delay = {1000, 1500},	-- miliseconds use fishing rod time
-	rod = 3483,				-- fishing rod id
+	rod = 3483,				-- fishinf rod id
+	minCap = 10,			-- dont fishing if cap below
 	positions = {{-5, 0}, {-6, 0}, {-7, 0}, {-5, 1}, {-6, 1}, {-7, 1}, {-5, -1}, {-6, -1}, {-7, -1}, {5, 0}, {6, 0}, {7, 0}, {5, -1}, {6, -1}, {7, -1}, {5, 1}, {6, 1}, {7, 1}}	-- where to fishing. Your pos in game is {0, 0}
 }
 
@@ -17,9 +18,10 @@ local selfloc, fishingPos = Self.Position(), {}
 Module.New("Fishing Through Window", function (mod)
 	if Self.isConnected() then
 		local pos = Self.Position()
-		
+		local cap = Self.Capity()
+
 		-- set random tile to fish.
-		if table.count(config.positions) > 0 then
+		if table.count(config.positions) > 0 and cap > config.minCap then
 			local tile = config.positions[math.random(1, table.count(config.positions))] 
 			Self.UseItemWithGround(config.rod, pos.x + tile[1], pos.y + tile[2], pos.z, 100) -- use fishing rod.
 		end	
