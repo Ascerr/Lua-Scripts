9ddc6ece5f59ba16c01abf9bed51ac28d87dcf16
Ascerr <rafalchabros1@gmail.com>
2021-08-07 19:27:13 +0200

    Update Auto Exori Con.lua

--- Auto Exori Con.lua
+++ Auto Exori Con.lua
@@ -1,40 +1,62 @@
 --[[
     Script Name: 		Auto Exori Con
-    Description: 		Cast exori con if target > 0 and hpperc > 50%
+    Description: 		Cast exori con if target > 0 and hpperc > 50% and depent on monsters on screen
     Author: 			Ascer - example
 ]]
 
-local SPELL = "exori con"   -- spell to cast
-local HPPERC = 50			-- don't cast if hp below %
-local MONSTERS = {"Dragon", "Dragon Lord", "Rat"} -- shoot only with in this monsters.
+
+local config = {
+    spell = {name = "exori con", mana = 25},                -- spell name and mana to cast
+    monsters = {"Dragon", "Dragon Lord", "Rat", "Cyclops"}, -- monsters list we cast spell
+    amount = 1,                                             -- minimal monsters amount on screen to cast spell
+    selfHpperc = 50,                                        -- dont cast if self hpperc below this value.
+}
+
 
 -- DON'T EDIT BELOW
-MONSTERS = table.lower(MONSTERS)
+config.monsters = table.lower(config.monsters)
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+--> Function:       getMonsters()
+--> Description:    Read monsters for param1 (count) and param2 (current attacked)
+--> Params:         None
+-->                
+--> Return:         integer - count, table (if true) - creature, 0 (if false)
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+function getMonsters()
+    local count, target, current = 0, Self.TargetID(), 0
+    for i, mob in ipairs(Creature.iMonsters(7, false)) do
+        if table.find(config.monsters, string.lower(mob.name)) then
+            count = count + 1
+            if target == mob.id then
+                current = mob
+            end    
+        end    
+    end
+    return count, current    
+end 
+
 
 -- module
 Module.New("Auto Exori Con", function (mod)
     
-	-- load self hpperc and targetid
-    local hp = Self.HealthPercent()
-    local target = Self.TargetID()
-
     -- when hp is ok and we have target
-    if hp > HPPERC and target > 0 then
+    if Self.HealthPercent() > config.selfHpperc and Self.Mana() >= config.spell.mana then
 
-    	-- load attacked creature
-        local mob = Creature.getCreatures(target)
-        
-        -- when name of this creature is in table.
-        if table.find(MONSTERS, string.lower(mob.name)) then
+        -- load count and target
+        local count, target = getMonsters()
 
+        -- when count is ok and is target.
+        if count >= config.amount and table.count(target) > 0 then
+    	
         	-- cast spell
-        	Self.CastSpell(SPELL)
+        	Self.CastSpell(config.spell.name)
 
-        end
-        	
+        end    
+
     end
 
-    -- execure delay
+    -- execute delay
     mod:Delay(300, 500)
 
 end)
