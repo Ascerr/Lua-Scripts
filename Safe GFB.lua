3e909ab3cf61b5635b9943be3bbed2d9029d6b9c
Ascerr <rafalchabros1@gmail.com>
2021-08-10 12:48:58 +0200

    Update Safe GFB.lua

--- Safe GFB.lua
+++ Safe GFB.lua
@@ -5,15 +5,19 @@
 ]]
 
 local AOE = {
-	amount = 4, 												-- minimal monsters amount to shoot rune
-	runeid = 3191, 												-- id of rune
-	monsters = {"Rat", "Snake"} 								-- type list of monsters, "*" to attack all
+	amount = 3, 												-- minimal monsters amount to shoot rune
+	runeid = 3161, 												-- id of rune
+	monsters = {"Rat", "Snake", "*"} 								-- type list of monsters, "*" to attack all
 }
 
 local SINGLE = {
-	rune = {enabled = true, id = 3198},							-- shoot rune enabled true/false, rune id
+	rune = {enabled = false, id = 3198},							-- shoot rune enabled true/false, rune id
 	spell = {enabled = false, name = "exori con", mana = 30}	-- cast spell enabled true/false, spell name
 }
+
+local MIN_SELF_HPERC = 50										-- dont cast spell/rune when hpperc below 50%
+local RUNE_DELAY = 2000											-- cast rune every (2000ms = 2s)
+local SPELL_DELAY = 1000										-- cast spell every (1000ms = 1s)
 
 					
 -- DONT EDIT BELOW THIS LINE
@@ -21,10 +25,10 @@ local SINGLE = {
 local FRIENDS = Rifbot.FriendsList(true)
 AOE.monsters = table.lower(AOE.monsters)
 
---> Area where we can shoot rune on screen
+--> [Area where we can shoot rune on screen]
 local runeMap = {{x=0,y=0}, {x=0,y=1}, {x=0,y=-1}, {x=1,y=0}, {x=-1,y=0}, {x=1,y=1}, {x=-1,y= 1}, {x=-1,y=-1}, {x=1,y=-1}, {x=2,y=0}, {x=-2,y=0}, {x=2,y=1}, {x=2,y=-1}, {x=-2,y=1}, {x=-2,y=-1}, {x=3,y=0}, {x=-3,y=0}, {x=3,y=1}, {x=3,y=-1}, {x=-3,y=1}, {x=-3,y=-1}}
 
---> Rune attack area
+--> [Rune attack area]
 local aoePos = {{0,0}, {0,1}, {0,-1}, {1,0}, {-1,0}, {-1,1}, {-1,-1}, {1,-1}, {1,1}, {-2,-1}, {-2,0}, {-2,1}, {2,-1}, {2,0}, {2,1}, {0,-2}, {-1,-2}, {1,-2}, {-2,-2}, {2,-2}, {0,2}, {-1,2}, {1,2}, {-2,2}, {2,2}, {0,-3}, {-1,-3}, {1,3}, {0,3}, {-1,3}, {1,3}, {-3,0}, {-3,-1}, {-3,1}, {3,0}, {3,-1}, {3,1}} 
 
 
@@ -208,13 +212,13 @@ function singleAttack()
 		if SINGLE.rune.enabled then
 
 			-- shoot rune with target (2s) delay.
-			Self.UseItemWithCreature(target, SINGLE.rune.id, 2000)
+			Self.UseItemWithCreature(target, SINGLE.rune.id, RUNE_DELAY)
 
 		-- when enable spell	
 		elseif SINGLE.spell.enabled then	
 
 			-- cast spell with target (1s) delay.
-			Self.CastSpell(SINGLE.spell.name, SINGLE.spell.mana, 1000)
+			Self.CastSpell(SINGLE.spell.name, SINGLE.spell.mana, SPELL_DELAY)
 
 		end
 
@@ -226,29 +230,34 @@ end
 -- Mod running with 200ms loop.
 Module.New("Safe GFB", function()
 
-	-- load monsters
-	local gfb, sd = safeGFB(AOE.monsters), false
+	-- when connected
+	if Self.isConnected() and Self.HealthPercent() >= MIN_SELF_HPERC then
 
-	-- when monsters fund
-	if gfb ~= false then
+		-- load monsters
+		local gfb, sd = safeGFB(AOE.monsters), false
+
+		-- when monsters fund
+		if gfb ~= false then
+			
+			-- when valid amount	
+			if gfb.amount >= AOE.amount then
+	        	
+				-- use rune with ground (2s delay)
+	        	Self.UseItemWithGround(AOE.runeid, gfb.x, gfb.y, Self.Position().z, RUNE_DELAY)
+
+	        else	
+
+	        	-- attack single
+	        	singleAttack()
+
+			end
+
+		else
 		
-		-- when valid amount	
-		if gfb.amount >= AOE.amount then
-        	
-			-- use rune with ground (2s delay)
-        	Self.UseItemWithGround(AOE.runeid, gfb.x, gfb.y, Self.Position().z, 2000)
+			-- attack single
+	        singleAttack()	
 
-        else	
-
-        	-- attack single
-        	singleAttack()
-
-		end
-
-	else
-	
-		-- attack single
-        singleAttack()	
+		end	
 
 	end	
 
