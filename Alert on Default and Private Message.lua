f0cc2410f80c5756310bdedc2ae3d68a102ce794
Ascerr <rafalchabros1@gmail.com>
2021-08-31 13:10:04 +0200

    Add files via upload

---
+++ Alert on Default and Private Message.lua
@@ -0,0 +1,20 @@
+--[[
+    Script Name:        Alert on Default and Private Message
+    Description:        Play sound if someone send message on default or private. Safe list = Friends.txt
+    Author:             Ascer - example
+]]
+
+
+local list = Rifbot.FriendsList(true)
+
+function proxy(messages) 
+	for i, msg in ipairs(messages) do 
+		if not table.find(list, string.lower(msg.speaker)) and msg.mode <= 5 and msg.channel <= 5 then
+			print(msg.speaker, msg.message, msg.channel, msg.mode, msg.level)
+			Rifbot.PlaySound("Default.mp3")
+		end	
+	end 
+end 
+
+-- register proxy message
+Proxy.New("proxy")
