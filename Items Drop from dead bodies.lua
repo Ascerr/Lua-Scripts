2965d529a4b75d4b0e0b9eff4dabdc60800eeeba
Ascerr <rafalchabros1@gmail.com>
2021-09-22 18:58:33 +0200

    Update Items Drop from dead bodies.lua

--- Items Drop from dead bodies.lua
+++ Items Drop from dead bodies.lua
@@ -5,7 +5,7 @@
 ]]
 
 local ITEMS = {3031, 3492}			-- items to drop
-local DROP_DELAY = {300, 1000} 		-- time between actions
+local DROP_DELAY = {200, 300} 		-- time between actions
 
 -- DON'T EDIT BELOW THIS LINE
 
@@ -13,7 +13,7 @@ Module.New("Drop Items", function ()
 	local item = Container.FindItem(ITEMS)
 	if item ~= false then
 		local pos = Self.Position()
-		Container.MoveItemToGround(item.index, item.slot, pos.x, pos.y, pos.z, item.id, item.count) -- this function at least parameter set default delay for move items check for more detail in Rifbot Lib.lua
+		Container.MoveItemToGround(item.index, item.slot, pos.x, pos.y, pos.z, item.id, item.count, 0)
 	end
 	mod:Delay(DROP_DELAY[1], DROP_DELAY[2])	
 end)
