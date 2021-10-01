6f4042326b481a852c50b9de81f4f20ae382aa6b
Ascerr <rafalchabros1@gmail.com>
2021-10-01 22:20:55 +0200

    Add files via upload

---
+++ Equip E ring if Monster Name.lua
@@ -0,0 +1,45 @@
+--[[
+    Script Name: 		Equip E ring if Monster Name
+    Description: 		When specific monsters on screen equip ering. 
+    Author: 			Ascer - example
+]]
+    
+local config = {
+    ring = {off = 3051, on = 3088},          -- rings id when {off} in bp, {on} in equipment
+    monsters = {
+        amount = 2,                         -- how many monsters detected on screen
+        names = {"Dragon Lord", "Demon"},   -- monsters names.
+    }
+}
+
+
+-- DON'T EDIT BELOW THIS LINE
+
+-- lower table
+config.monsters.names = table.lower(config.monsters.names)
+
+Module.New("Equip E ring if Monster Name", function ()
+    
+    -- set count
+    local count = 0
+
+    -- in loop check for monsters
+    for i, mob in pairs(Creature.iMonsters(7)) do
+        if table.find(config.monsters.names, string.lower(mob.name)) then
+            count = count + 1
+            if count >= PLAYERS then
+                if Self.Ring().id ~= config.ring.on then
+                    Self.EquipItem(config.ring.off, RING, 1)
+                end    
+            end    
+        end
+    end
+    
+    -- check for dequip
+    if count < PLAYERS then
+        if Self.Ring().id == config.ring.on then
+            Self.DequipItem(SLOT_RING)
+        end    
+    end  
+    
+end)
