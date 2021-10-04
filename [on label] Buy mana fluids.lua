8a07e79a7ba40aeeccd9356ec154f84d4007f375
Ascerr <rafalchabros1@gmail.com>
2021-10-04 12:18:33 +0200

    Update [on label] Buy mana fluids.lua

--- [on label] Buy mana fluids.lua
+++ [on label] Buy mana fluids.lua
@@ -5,8 +5,9 @@
 ]]
 
 local config = {
-    check = {label = "check", goto = "back", amount = 30},      -- when label "check" then if amount of vials <= 10 goto label "back"
-    refill = {label = "shop", amount = 100}                     -- when label shop make refill (buy up to 30 vials)
+    check = {label = "check", goto = "back", amount = 5},      -- when label "check" then if amount of vials <= 5 goto label "back" (minimal amount is 2 to verify message)
+    refill = {label = "shop", amount = 30},                    -- when label shop make refill (buy up to 30 vials)
+    dropEmptyVialsFromMainBp = true                            -- throw empty vials from main backpack under ground 
 }
 
 
@@ -18,7 +19,7 @@ function signal(label)
     if label == config.refill.label then
         buyFluidsUpTo(config.refill.amount)
     elseif label == config.check.label then
-        if currentVials < config.check.amount then
+        if currentVials <= config.check.amount then
             Walker.Goto(config.check.goto)
         end    
     end
@@ -44,7 +45,11 @@ Proxy.TextNew("proxyText")
 --> Return:         boolean true or nil
 ----------------------------------------------------------------------------------------------------------------------------------------------------------
 function buyFluidsUpTo(amount)
-    Self.Say("instant sell vials")
+    Self.Say("hi")
+    wait(500)
+    Self.Say("sell vials")
+    wait(500)
+    Self.Say("yes")
     Self.UseItemWithMe(MANA_FLUID.id, 0)
     wait(1000)
     while true do
@@ -56,3 +61,34 @@ function buyFluidsUpTo(amount)
         wait(1500, 2000)
     end    
 end
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+--> Function:       dropEmptyVials()
+--> Description:    Drop empty vials from container index 0 and arrow slot to ground under your character
+--> Params:         None
+--> Return:         boolean true or false
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+function dropEmptyVials()
+    local items = Container.getItems(0)
+    for i, item in pairs(items) do
+        if item.id == MANA_FLUID.id and item.count == 0  then
+            local pos = Self.Position()
+            return Container.MoveItemToGround(0, i-1, pos.x, pos.y, pos.z, item.id, 1, 500) 
+        end
+    end
+    local arrow = Self.Ammo()
+    if arrow.id == MANA_FLUID.id and arrow.count == 0 then
+        local pos = Self.Position()
+        return Container.MoveItemFromEquipmentToGround(SLOT_AMMO, pos.x, pos.y, pos.z, arrow.id, 1, 500)
+    end
+    return false    
+end    
+
+
+--> Module for drop empty vials from main backpack and arrow slot.
+Module.New("Drop empty vials from main backpack", function(mod)
+    if config.dropEmptyVialsFromMainBp then
+        dropEmptyVials()
+    end    
+    mod:Delay(300, 700)    
+end)
