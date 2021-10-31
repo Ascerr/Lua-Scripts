19be91ef13de47ae83496b1afef85370844c891a
Ascerr <rafalchabros1@gmail.com>
2021-10-31 11:42:10 +0100

    Add files via upload

---
+++ [on Shortkey] switch arrow in slot.lua
@@ -0,0 +1,21 @@
+--[[
+    Script Name:        [on Shortkey] switch arrow in slot
+    Description:        Switch arrow depent on current in slot, its shortkey usage script type: EXECUTE name
+    Author:             Ascer - example
+]]
+
+
+local config = {
+    arrow = 3447,               -- id o basic arrow
+    hunting_arrow = 1111,       -- id of hunting arrow or other
+}
+
+local ammo = Self.Ammo()
+
+if ammo.id == 0 then
+    Self.EquipItem(SLOT_AMMO, config.arrow, 1, 200)
+elseif ammo.id == config.arrow then
+    Self.EquipItem(SLOT_AMMO, config.hunting_arrow, count, 200)
+else
+    Self.EquipItem(SLOT_AMMO, config.arrow, count, 200)
+end 
