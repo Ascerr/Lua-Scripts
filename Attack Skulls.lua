34b92457e4ff7b5205b1d5961035023e68b0487e
Ascerr <rafalchabros1@gmail.com>
2021-11-18 15:58:03 +0100

    Update Attack Skulls.lua

--- Attack Skulls.lua
+++ Attack Skulls.lua
@@ -7,13 +7,13 @@
 local IGNORE_LIST = {"player1", "player2"} 			-- list of players to ignore with Capital letter.
 local RANGE = 7					     				-- max distance to attack creatures
 local SHOOT_RUNE = {enabled = false, runeid = 2311}	-- attack with runes: true/false, rune id
+local AVOID_SHOOTING_WITH_STACKED_PLAYERS = true	-- if players are stacked on sqm like stairs or ladders avoid shooting if don't have skull.
 
 -- DON'T EDIT BELOW THIS LINE
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------
 --> Function:		getCreatureWithSkull(list, range)
 --> Description: 	Get table with creature to attack.
---> Class: 			None
 --> Params:			
 -->					@list table with creature names
 -->					@range number max distance between you and creature
@@ -75,6 +75,45 @@ function getCreatureWithSkull(list, range)
 
 end
 
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+--> Function:		isMoreThanOneCreatureOnStairs(c)
+--> Description: 	Check if is some player on stack with current target we have.
+--> Params:			
+-->					@list table with creature names
+-->					@c table with creature
+--> Return: 		boolean true or false
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+function isMoreThanOneCreatureOnStairs(list, c)
+
+	-- read creatures
+    local creatures = Creature.iPlayers(9, false) 
+
+    -- in loop for creatures.
+    for i = 1, #creatures do
+
+    	-- load single creature.
+    	local creature = creatures[i]
+
+    	-- check for pos
+    	if c.x == creature.x and c.y == creature.y and c.z == creature.z then
+
+	    	-- check if valid name
+	    	if not table.find(list, creature.name) and not (creature.skull == SKULL_WHITE or creature.skull == SKULL_RED) then
+
+	    		-- we found creature return.
+	    		return true
+
+	    	end
+
+	    end	
+    	
+    end
+
+    -- no creature
+    return false
+
+end	
+
 
 -- module to run function
 Module.New("Attack Skulls", function (mod)
@@ -102,8 +141,19 @@ Module.New("Attack Skulls", function (mod)
 					-- when shooting runes is enabled use rune
 					if SHOOT_RUNE.enabled and Creature.isPlayer(creature) then
 
+						-- set param false for stacked players
+						local stacked = false
+
+						-- when we check for stacked players
+						if AVOID_SHOOTING_WITH_STACKED_PLAYERS then
+
+							-- load stacked
+							stacked = isMoreThanOneCreatureOnStairs(IGNORE_LIST, creature)
+
+						end	
+
 						-- use rune
-						Self.UseItemWithCreature(creature, SHOOT_RUNE.runeid, 1000)
+						if not stacked then Self.UseItemWithCreature(creature, SHOOT_RUNE.runeid, 1000) end
 
 					end	
 
