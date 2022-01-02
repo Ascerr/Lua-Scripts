03ff02eae15ea14563a78af2db07fcc75262e1b0
Ascerr <rafalchabros1@gmail.com>
2022-01-02 16:58:03 +0100

    Update Safe Exori.lua

--- Safe Exori.lua
+++ Safe Exori.lua
@@ -5,27 +5,32 @@
 ]]
 
 local EXORI = "exori"					-- name of spell to cast
-local MANA = 200						-- min mana need to cast spell
+local MANA = 20						    -- min mana need to cast spell
 local FRIENDS = {"Friend1", "Friend2"} 	-- list of friends we can use exori Capital letters like a Character Name
-local MONSTERS_AMOUNT_TO_USE = 4		-- min monsters amount to cast spell
+local MONSTERS = {"Demon", "Dragon"}     -- monsters list to attack.
+local MONSTERS_AMOUNT_TO_USE = 1		-- min monsters amount to cast spell
 local DONT_CAST_WHEN_PLAYER_DIST = 2	-- don't use spell when player distance from self is equal or below this sqms.
 local SELF_MIN_HPPERC = 70              -- don't cast spell if your character health percent is below this value
+
 
 -- DON'T EDIT BELOW THIS LINE.
 
 local useSpellTime = 0
+local FRIENDS = table.lower(FRIENDS)
+local MONSTERS = table.lower(MONSTERS)
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------
---> Function:		ableToCastExori(friends, monstersAmount, playerRange)
+--> Function:		ableToCastExori(friends, monstersAmount, monsters, playerRange)
 --> Description: 	Check if character is able to cast exori spell in current situation.
 --> Class: 			None
 --> Params:			
 -->					@friends table with friends, capital letters like a Character Name
+-->                 @monsters table with creature names
 -->					@monstersAmount number minimal monster amount around as to cast spell
 -->					@playerRange number distance between player and you to don't cast spell. (equal or below)
 --> Return: 		boolean true or false.		
 ----------------------------------------------------------------------------------------------------------------------------------------------------------
-function ableToCastExori(friends, monstersAmount, playerRange)
+function ableToCastExori(friends, monstersAmount, monsters, playerRange)
 	
 	-- set creature count for 0.
 	local count = 0
@@ -46,15 +51,15 @@ function ableToCastExori(friends, monstersAmount, playerRange)
         if Creature.isPlayer(c) then
 
         	-- if not find friends in table
-            if not table.find(FRIENDS, c.name) then
+            if not table.find(friends, string.lower(c.name)) then
 
             	-- return false we can't use exori due player.
             	return false
                 
             end
 
-        -- if creature is monster count amount.   
-        elseif Creature.isMonster(c) then
+        -- if creature is monster count amount and is valid name.   
+        elseif Creature.isMonster(c) and table.find(monsters, string.lower(c.name)) then
 
         	-- if creature distance from self is 1 and hpperc is above 0.
         	if math.abs(c.x - pos.x) <= 1 and math.abs(c.y - pos.y) <= 1 and math.abs(c.z - pos.z) == 0 and c.hpperc > 0 then
@@ -91,7 +96,7 @@ Module.New("Safe Exori", function ()
 		if os.time() - useSpellTime >= 2 then
 
 			-- check if we can shoot exori.
-			if ableToCastExori(FRIENDS, MONSTERS_AMOUNT_TO_USE, DONT_CAST_WHEN_PLAYER_DIST) then
+			if ableToCastExori(FRIENDS, MONSTERS_AMOUNT_TO_USE, MONSTERS, DONT_CAST_WHEN_PLAYER_DIST) then
 
 				-- say spell
 				Self.Say(EXORI)
