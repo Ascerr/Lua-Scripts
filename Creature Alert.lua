72ba8ac65a0f58243a52abe63c6d34faf97ffb5d
Ascerr <rafalchabros1@gmail.com>
2022-03-12 13:50:09 +0100

    Update Creature Alert.lua

--- Creature Alert.lua
+++ Creature Alert.lua
@@ -4,6 +4,7 @@
     Author:             Ascer - example
 ]]
 
+local AMOUNT = 1                                 -- amount of monsters to play alert
 local DETECT = 2                                 -- detect creature type, 0 - player, 1 - monster, 2 - both 
 local SAFE_LIST = {"Defense Monster", "Friend2"} -- Safe list. Names need to by with Capital letter.
 
@@ -24,18 +25,29 @@ Module.New("Creature Alert", function ()
         c = "creatures"
 
     end    
-        
+    
+    -- set default count
+    local count = 0
+
     -- inside loop for creatures
     for i, cre in pairs(Creature.iFunction(c, 7, false)) do
         
         -- when creature is not friend and we chek only for players/monsters or both and creature is not NPC
         if not table.find(SAFE_LIST, cre.name) and (DETECT < 2 or not Creature.isNpc(cre)) then
             
-            -- play sound for more search in Rifbot Lib.lua -> RIFBOT_SOUNDS = []
-            playSound("Creature Detected.mp3") 
-            
-            -- break loop
-            break 
+            -- add count
+            count = count + 1
+
+            -- play sound only if enough count
+            if count >= AMOUNT then
+
+                -- play sound for more search in Rifbot Lib.lua -> RIFBOT_SOUNDS = []
+                playSound("Creature Detected.mp3") 
+                
+                -- break loop
+                break 
+
+            end    
         
         end
     
