b3a17a230a9f79e3743fa843bec05cdccfbefc2c
Ascerr <rafalchabros1@gmail.com>
2022-04-16 09:36:51 +0200

    Update Open Next Bp when full.lua

--- Open Next Bp when full.lua
+++ Open Next Bp when full.lua
@@ -4,9 +4,13 @@
     Author:             Ascer - example
 ]]
 
+local BACKPACKS = {
+    {index = 0, id = 2854},     -- container index 0 = first opened container, id - backpack id to open.
+    {index = 1, id = 2854}
+    -- you can add more here
+}
 
 -- DONT EDIT BELOW THIS LINE
-local BACKPACK = {index = 0, id = 2853}
 
 -- loop module
 Module.New("Open Next Bp when full", function (mod)
@@ -14,30 +18,35 @@ Module.New("Open Next Bp when full", function (mod)
     -- when connected.
     if Self.isConnected() then
 
-        -- load container with index: BACKPACK.index
-        local cont = Container.getInfo(BACKPACK.index)
+        -- inside loop
+        for i, bp in ipairs(BACKPACKS) do
 
-        -- when amount of items in table is above 0
-        if table.count(cont) > 0 then
+            -- load container with index: BACKPACK.index
+            local cont = Container.getInfo(bp.index)
 
-            -- when amount of items == size of cont.
-            if cont.amount == cont.size then
+            -- when amount of items in table is above 0
+            if table.count(cont) > 0 then
 
-                -- searach for next backpack to open.
-                local item = Container.FindItem(BACKPACK.id, BACKPACK.index)
+                -- when amount of items == size of cont.
+                if cont.amount == cont.size then
 
-                -- if found
-                if item ~= false then
- 
-                    -- open backpack
-                    Container.UseItem(item.index, item.slot, item.id, false, 1000)
+                    -- searach for next backpack to open.
+                    local item = Container.FindItem(bp.id, bp.index)
 
-                end 
+                    -- if found
+                    if item ~= false then
+     
+                        -- open backpack
+                        Container.UseItem(item.index, item.slot, item.id, false, 1000)
 
-            end    
+                    end 
 
-        end
+                end    
 
+            end
+
+        end    
+            
     end    
 
     -- delay between actions in miliseconds
