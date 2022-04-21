602fee268d59cada7796b4f87bfccb5196462e74
Ascerr <rafalchabros1@gmail.com>
2022-04-21 08:08:10 +0200

    Add files via upload

---
+++ Fast SSA & Might Ring.lua
@@ -0,0 +1,14 @@
+--[[ 
+    Script Name: 		Fast Auto SSA
+    Description: 		Spam equipming Stone Skin Amulet (delay 200ms).
+    Author: 			Ascer - example
+]]
+
+local SSA = 3081	-- id of stone skin amulet
+local MRING = 3048	-- id of might ring
+
+Module.New("Fast SSA & Might Ring", function (mod) -- declare module name and set function mod
+    Self.EquipItem(SLOT_AMULET, SSA, 1, 1) -- equip stone skin amulet to amulet slot with quantity 1.
+    wait(100)
+    Self.EquipItem(SLOT_RING, MRING, 1, 1) -- equip might ring
+end)
