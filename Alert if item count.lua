8705b17119b987eb27756a4fb48f29b3711c54a8
Ascerr <rafalchabros1@gmail.com>
2022-05-24 19:35:22 +0200

    Update Alert if item count.lua

--- Alert if item count.lua
+++ Alert if item count.lua
@@ -8,32 +8,12 @@ local ITEM = {id = 2853, count = 2}  -- item.id and item.count.
 
 -- DON'T EDIT BELOW THIS LINE.
 
-----------------------------------------------------------------------------------------------------------------------------------------------------------
---> Function:       getItemCount(itemid)
---> Description:    Read containers and equipment for count of items.
---> Class:          None
---> Params:         @itemid int which id we looking foor
---> Return:         int amount of items
-----------------------------------------------------------------------------------------------------------------------------------------------------------
-function getItemCount(itemid)
-    local count = Self.ItemCount(itemid)
-    for i=0, 9 do
-        local slot = selfGetEquipmentSlot(i)
-        if slot.id == itemid then
-            count = count + slot.count
-        end    
-    end
-    return count 
-end
-
 
 -- Module to run functions in loop 200ms.
 Module.New("Alert if item count", function ()
     if Self.isConnected() then
-        if getItemCount(ITEM.id) < ITEM.count then
+        if Self.ItemCount(ITEM.id) < ITEM.count then
             Rifbot.PlaySound("Default.mp3")
         end
     end        
 end)
-
-
