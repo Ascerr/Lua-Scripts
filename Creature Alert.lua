f6246f9840eaae0e4b2bfb96c07494bd47fc6d21
Ascerr <rafalchabros1@gmail.com>
2022-05-31 16:48:54 +0200

    Update Creature Alert.lua

--- Creature Alert.lua
+++ Creature Alert.lua
@@ -4,9 +4,10 @@
     Author:             Ascer - example
 ]]
 
-local AMOUNT = 1                                 -- amount of monsters to play alert
-local DETECT = 2                                 -- detect creature type, 0 - player, 1 - monster, 2 - both 
-local SAFE_LIST = {"Defense Monster", "Friend2"} -- Safe list. Names need to by with Capital letter.
+local AMOUNT = 1                                                    -- amount of monsters to play alert
+local DETECT = 2                                                    -- detect creature type, 0 - player, 1 - monster, 2 - both 
+local SAFE_LIST = {"Defense Monster", "Friend2"}                    -- Safe list. Names need to by with Capital letter.
+local SPECIAL_LIST = {enabled = false, names = {"Rat", "Snake"}}    -- enabled = true/false if you want check for special names. Use Capital letter.
 
 Module.New("Creature Alert", function ()
     
@@ -35,20 +36,28 @@ Module.New("Creature Alert", function ()
         -- when creature is not friend and we chek only for players/monsters or both and creature is not NPC
         if not table.find(SAFE_LIST, cre.name) and (DETECT < 2 or not Creature.isNpc(cre)) then
             
-            -- add count
-            count = count + 1
+            -- check if is enabled checking for special list
+            local var = (not SPECIAL_LIST.enabled or (SPECIAL_LIST.enabled and table.find(SPECIAL_LIST.names, cre.name)))
 
-            -- play sound only if enough count
-            if count >= AMOUNT then
+            -- check if var is valid.
+            if var then
 
-                -- play sound for more search in Rifbot Lib.lua -> RIFBOT_SOUNDS = []
-                playSound("Creature Detected.mp3") 
-                
-                -- break loop
-                break 
+                -- add count
+                count = count + 1
 
+                -- play sound only if enough count
+                if count >= AMOUNT then
+
+                    -- play sound for more search in Rifbot Lib.lua -> RIFBOT_SOUNDS = []
+                    playSound("Creature Detected.mp3") 
+                    
+                    -- break loop
+                    break 
+
+                end    
+            
             end    
-        
+
         end
     
     end  
