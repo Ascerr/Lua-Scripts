81920354b6085595c34d9aedccf38eadbf430f70
Ascerr <rafalchabros1@gmail.com>
2022-06-10 14:44:12 +0200

    Update Message responder.lua

--- Message responder.lua
+++ Message responder.lua
@@ -7,20 +7,30 @@
 local SPPED_REACTION_FOR_MESSAGE = 500			-- miliseconds wait before start typing message
 local SPEED_PER_LETTER = 90						-- miliseconds we spent for write one single letter
 local RESPOND_TO = {"Player", "GM"}				-- respond for messages from this players (nick names)
-local RANDOM_RESPOND_WORDS = {"siema byku", "yo", "ello", "hiho", "yoyo"}		-- use thi random words to respond
+local RESPOND_TO_EVERYONE = false				-- true/false respond for every possible nick.
+local STOP_CAVEBOT_WHEN_RESPONDING = false		-- while responding just stop cavebot for while.
+local RANDOM_RESPOND_WORDS = {"siema byku", "yo", "ello", "hiho", "yoyo"}		-- use the random words to respond
 
 -- DONT EDIT BELOW THIS LINE
 
-local orders = {}
+local orders, responded = {}, {Self.Name()}
 RESPOND_TO = table.lower(RESPOND_TO)
 
 function proxy(messages) 
 	for i, msg in ipairs(messages) do 
-		if table.find(RESPOND_TO, string.lower(msg.speaker)) then
-			createOrder(RANDOM_RESPOND_WORDS[math.random(1, #RANDOM_RESPOND_WORDS)])
-			Rifbot.PlaySound()
-			print(msg.speaker, msg.message, msg.channel, msg.mode, msg.level) 
-		end	
+		if msg.channel < 2 or msg.mode < 2 then
+			if table.find(RESPOND_TO, string.lower(msg.speaker)) or RESPOND_TO_EVERYONE then
+				if not table.find(responded, msg.speaker) then
+					table.insert(responded, msg.speaker)
+					createOrder(RANDOM_RESPOND_WORDS[math.random(1, #RANDOM_RESPOND_WORDS)])
+					Rifbot.PlaySound()
+					if STOP_CAVEBOT_WHEN_RESPONDING then
+						Cavebot.Enabled(false)
+					end	
+				end	
+				print(msg.speaker, msg.message, msg.channel, msg.mode, msg.level) 
+			end
+		end		
 	end 
 end 
 
@@ -29,6 +39,10 @@ function executeOrder()
 		if os.clock() - order.time > (order.delay / 1000) then
 			Self.Say(order.text)
 			table.remove(orders, i)
+			wait(500, 900)
+			if STOP_CAVEBOT_WHEN_RESPONDING then
+				Cavebot.Enabled(true)
+			end	
 			return true
 		end	
 	end	
@@ -44,8 +58,11 @@ Module.New("Message responder", function()
 	executeOrder()
 end)
 
+-- module to remove already responded players every 2h
+Module.New("Remove storage", function(mod)
+	responded = {Self.Name()}
+	mod:Delay(1000*60*60*2)
+end)
+
 -- register new proxy
 Proxy.New("proxy")
-
-
-
