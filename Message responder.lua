b9ec0a71afaea66d21640215c117a16705c19abc
Ascerr <rafalchabros1@gmail.com>
2022-09-27 11:11:37 +0200

    Update Message responder.lua

--- Message responder.lua
+++ Message responder.lua
@@ -1,13 +1,13 @@
 --[[
     Script Name: 		Message responder
-    Description: 		Respond for message on chat
+    Description: 		Respond both on default and PMs for incomming key messages. 
     Author: 			Ascer - example
 ]]
 
 local SPPED_REACTION_FOR_MESSAGE = 500			-- miliseconds wait before start typing message
 local SPEED_PER_LETTER = 90						-- miliseconds we spent for write one single letter
-local STOP_CAVEBOT_WHEN_RESPONDING = false		-- while responding just stop cavebot for while.
-local RESOPND_PRIVATE_MESSAGE_ONLY = false		-- respond only for private message sendig pms too.
+local STOP_CAVEBOT_WHEN_RESPONDING = true		-- while responding just stop cavebot for while.
+local PLAY_SOUND = true							-- play sound on message appear.
 local RESPOND_ONLY_TO_THIS = { 
 	enabled = false,							-- enable true/false 
 	nicks = {"Player", "GM"}					-- respond for messages from this players (nick names)
@@ -23,37 +23,43 @@ local RESPOND_KEYWORDS = {
 
 -- DONT EDIT BELOW THIS LINE
 
-local orders, responded = {}, {Self.Name()}
+local orders, responded, me = {}, {}, Self.Name()
 RESPOND_ONLY_TO_THIS.nicks = table.lower(RESPOND_ONLY_TO_THIS.nicks)
+
 
 function proxy(messages) 
 	for i, msg in ipairs(messages) do 
-		if ((not RESOPND_PRIVATE_MESSAGE_ONLY and msg.channel < 2 and msg.mode < 8) or (RESOPND_PRIVATE_MESSAGE_ONLY and msg.channel == 0 and msg.mode == 4)) then 
+		if msg.channel < 2 and msg.mode < 8 and msg.speaker ~= me then 
 			msg.message = string.lower(msg.message)
 			if (RESPOND_ONLY_TO_THIS.enabled and table.find(RESPOND_ONLY_TO_THIS.nicks, string.lower(msg.speaker))) or not RESPOND_ONLY_TO_THIS.enabled then
-				if not table.find(responded, msg.speaker) then
-					local respond = keywordFound(msg.message)
-					if respond ~= nil then
-						if not RESOPND_PRIVATE_MESSAGE_ONLY then 
-							table.insert(responded, msg.speaker)
-						end	
-						createOrder(respond, msg.speaker)
-					end
-					if STOP_CAVEBOT_WHEN_RESPONDING then
-						Cavebot.Enabled(false)
-					end		
-					Rifbot.PlaySound()
+				
+				local name = nil
+				if msg.channel == 0 and msg.mode == 4 then name = msg.speaker end
+
+				local respond, key = keywordFound(msg.message)
+
+				if respond ~= nil and not playerKeyExists(msg.speaker, key) then
+					createOrder(respond, name)
+					updatePlayerKey(msg.speaker, key)
+				end
+
+				if STOP_CAVEBOT_WHEN_RESPONDING then
+					Cavebot.Enabled(false)
 				end	
+
+				if PLAY_SOUND then Rifbot.PlaySound() end
+					
 				print(msg.speaker, msg.message, msg.channel, msg.mode, msg.level) 
 			end
 		end		
 	end 
 end 
+Proxy.New("proxy")
 
 function executeOrder()
 	for i, order in ipairs(orders) do
 		if os.clock() - order.time > (order.delay / 1000) then
-			if RESOPND_PRIVATE_MESSAGE_ONLY then
+			if order.player ~= nil then
 				Self.PrivateMessage(order.player, order.text, 0)
 			else	
 				Self.Say(order.text)
@@ -73,15 +79,39 @@ function createOrder(msg, player)
 	table.insert(orders, order)
 end	
 
+function playerKeyExists(player, index)
+	for i, key in ipairs(responded) do
+		if key.name == player then
+			if table.find(key.number, index) then
+				return true
+			end
+		end
+	end			
+	return false
+end	
+
+function updatePlayerKey(player, index)
+	for i, key in ipairs(responded) do
+		if key.name == player then
+			if not table.find(key.number, index) then
+				table.insert(key.number, index)
+				responded[i] = key
+				return
+			end	
+		end	
+	end
+	table.insert(responded, {name = player, number = {index}})	
+end	
+
 function keywordFound(message)
 	for i, element in ipairs(RESPOND_KEYWORDS) do
 		for j, word in ipairs(element.key) do
 			if string.find(message, "%f[%a]" .. word .. "%f[%A]") or word == "*" then
-				return element.respond[math.random(1, #element.respond)]
+				return element.respond[math.random(1, #element.respond)], i
 			end	
 		end
 	end
-	return nil	
+	return nil, nil	
 end	
 
 -- module to run in loop
@@ -91,12 +121,9 @@ end)
 
 -- module to remove already responded players every 2h
 Module.New("Remove storage", function(mod)
-	responded = {Self.Name()}
+	responded = {}
 	mod:Delay(1000*60*60*2)
 end)
-
--- register new proxy
-Proxy.New("proxy")
 
 
 
