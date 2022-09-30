fcd2ad47b9b4b4472a43a66f3dc315d681e29961
Ascerr <rafalchabros1@gmail.com>
2022-09-30 21:23:08 +0200

    Update Safe Manaburner.lua

--- Safe Manaburner.lua
+++ Safe Manaburner.lua
@@ -5,10 +5,11 @@
 ]]
 
 local MANA_BURNER = {
-    spell = "exura",                            -- spell to cast
-    when_mana_between = {25, 27},               -- cast spell only when mana will between this values.
-    play_sound_if_mana_above = false,           -- will play sound when mana will above this values (GM charging you or you make wrong calculation about mana regen)
-    pause_casting_if_mana_decreased_for = 20     -- don't cast spell for next 100s if we deteced mana decreasing (you already casted spell)
+    spell = "adevo grav flam",                          -- spell to cast
+    when_mana_between = {240, 250},                     -- cast spell only when mana will between this values.
+    pause_casting_if_mana_decreased_for = 20,           -- don't cast spell for next 100s if we deteced mana decreasing (you already casted spell)
+    equip_blank_rune = {enabled = false, id = 3147},    -- equip blank rune to hand before rune make enabled - true/false, id of blank rune 
+    ping = 1000                                         -- time in miliseconds between key functions like equip rune or cast spell
 }
 
 local ANTI_IDLE = {
@@ -20,7 +21,7 @@ local ANTI_IDLE = {
 -- DON'T EDIT BELOW
 local antiidleTime, antiidleDelay, antiidleDir, antiidleRandDir, antiidleRestore = 0, math.random(ANTI_IDLE.delay[1], ANTI_IDLE.delay[2]), -1, -1, false
 local lastMana, manaburnerPause, manaburnerTime = Self.Mana(), false, 0
-
+local blankPos, blankBackRune = {index = -1, slot = -1, id = -1, count = -1}, false
 
 function manaburner()
     local mana = Self.Mana()
@@ -34,14 +35,18 @@ function manaburner()
     end    
     if lastMana - mana > 0 then
         manaburnerPause = true
+        blankBackRune = true
         manaburnerTime = os.clock()
-        print("[Manaburner] Pause spell caster for: " .. MANA_BURNER.pause_casting_if_mana_decreased_for .. " due mana decreased.") 
+        print("[Manaburner] Pause spell caster for: " .. MANA_BURNER.pause_casting_if_mana_decreased_for .. "sec due mana decreased.") 
         return
     end
     lastMana = mana 
     if mana >= MANA_BURNER.when_mana_between[1] and mana <= MANA_BURNER.when_mana_between[2] then
+        if MANA_BURNER.equip_blank_rune.enabled then
+            if not wearBlankRune() then return end
+        end    
         Self.Say(MANA_BURNER.spell)
-        wait(1000)
+        wait(MANA_BURNER.ping)
     elseif mana > MANA_BURNER.when_mana_between[2] then
         Rifbot.PlaySound()
         print("[Manaburner] Detected mana above limit: " .. mana .. " while limit is " .. MANA_BURNER.when_mana_between[2])    
@@ -86,10 +91,62 @@ function antiidle()
     end    
 end 
 
+function wearBlankRune()
+    if MANA_BURNER.equip_blank_rune.enabled then
+        local weapon = Self.Weapon()
+        if weapon.id == 0 then
+            local blank = Container.FindItem(MANA_BURNER.equip_blank_rune.id)
+            if table.count(blank) > 2 then
+                blankPos = blank
+                Container.MoveItemToEquipment(blank.index, blank.slot, SLOT_WEAPON, blank.id, 1, 0)
+                wait(MANA_BURNER.ping)
+                return false
+            else
+                Rifbot.PlaySound()
+                print("[Manaburner]: There is no more blank runes.")
+                return false
+            end 
+        elseif weapon.id ~= MANA_BURNER.equip_blank_rune.id then
+            Self.DequipItem(SLOT_WEAPON)
+        else
+            if blankPos.index == -1 then
+                local blank = Container.FindItem(MANA_BURNER.equip_blank_rune.id)
+                if table.count(blank) > 2 then
+                    blankPos = blank
+                end    
+            end    
+            return true
+        end   
+    end    
+end    
+
+function putBackBlankRune()
+    if not blankBackRune or not MANA_BURNER.equip_blank_rune.enabled then return end
+    if blankPos.index ~= -1 then
+        local weapon = Self.Weapon()
+        if weapon.id ~= MANA_BURNER.equip_blank_rune.id and weapon.id ~= 0 then
+            local dest = Container.getInfo(blankPos.index)
+            if (table.count(dest) < 2 or (table.count(dest) >= 2 and dest.size == dest.amount)) then
+                Self.DequipItem(SLOT_WEAPON)
+            else
+                Self.DequipItem(SLOT_WEAPON, blankPos.index, dest.size - 1)
+            end    
+        else
+            blankBackRune = false
+            blankPos = {index = -1, slot = -1, id = -1, count = -1}
+            return true
+        end        
+    else
+        blankBackRune = false
+        blankPos = {index = -1, slot = -1, id = -1, count = -1}
+        return true    
+    end    
+end    
 
 Module.New("Safe Manaburner", function()
     if Self.isConnected() then
         manaburner()
         antiidle()
+        putBackBlankRune()
     end    
 end)
