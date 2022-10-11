9957da9ba77b90521e807520a7d494c43b563371
Ascerr <rafalchabros1@gmail.com>
2022-10-11 17:28:56 +0200

    Update Switch Life Ring and ROH.lua

--- Switch Life Ring and ROH.lua
+++ Switch Life Ring and ROH.lua
@@ -6,7 +6,7 @@
 
 local LIFE_RING = {on = 3089, off = 3052}           -- set id for life ring on - equiped, off - not
 local RING_OF_HEALING = {on = 3098, off = 3061}     -- set id for ring of healing on - equiped, off - not
-local MANA = 700                                    -- if mana below this value equip roh else life ring.
+local MANA = {on = 700, off = 1100}                 -- if mana below ON value equip roh and life ring when mana above OFF.
 
 -- DON'T EDIT BELOW THIS LINE 
 
@@ -15,8 +15,11 @@ Module.New("Switch Life Ring and ROH ", function ()
     -- when connected
     if Self.isConnected() then
 
+        -- load mana
+        local mp = Self.Mana()
+
         -- when mana
-        if Self.Mana() <= MANA then    
+        if mp <= MANA.on then    
         
             -- when our slot.ring.id is different than RING_OF_HEALING.on
             if Self.Ring().id ~= RING_OF_HEALING.on then
@@ -28,11 +31,19 @@ Module.New("Switch Life Ring and ROH ", function ()
 
         else
 
-            -- when our slot.ring.id is different than life_ring.on
-            if Self.Ring().id ~= LIFE_RING.on then
+            -- load ring
+            local ring = Self.Ring().id
 
-                -- equip ring 0 delay.
-                Self.EquipItem(SLOT_RING, LIFE_RING.off, 1, 0)
+            -- when mana is above off roh or no ring 
+            if mp > MANA.off or ring == 0 then
+
+                -- when our slot.ring.id is different than life_ring.on
+                if ring ~= LIFE_RING.on then
+
+                    -- equip ring 0 delay.
+                    Self.EquipItem(SLOT_RING, LIFE_RING.off, 1, 0)
+
+                end
 
             end
 
