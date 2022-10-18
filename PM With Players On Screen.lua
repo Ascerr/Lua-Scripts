5a0ff6ed82366ec1bb38743a95feff7bac35a072
Ascerr <rafalchabros1@gmail.com>
2022-10-18 11:56:37 +0200

    Update PM With Players On Screen.lua

--- PM With Players On Screen.lua
+++ PM With Players On Screen.lua
@@ -4,7 +4,7 @@
     Author: 			Ascer - example
 ]]
 
-local RECEIVER = "Cipfried"                     -- character to receive messages.
+local RECEIVERS = {"Receiver1", "Receiver2"}               -- characters to receive messages.
 local SAFE_LIST = {"friend1", "friend2"}        -- avoid sending message with your friends
 local INSTANT = true                            -- send message always or only when a new player enter on screen
 local SPEAK_DELAY = 3                           -- time in seconds between sending msg.
@@ -12,6 +12,25 @@ local SPEAK_DELAY = 3                           -- time in seconds between sendi
 -- DON'T EDIT BELOW THIS LINE
 
 local storage, speakTime = {}, 0
+local sayStartTime, sayLastTime, sayMessageCount = 0, 0, 0
+
+function pm(player, text, delay)
+    if not delay then delay = 2000 end
+    if os.clock() - sayStartTime >= 10 then
+        sayMessageCount = 0
+        sayStartTime = 0
+    end        
+    if sayMessageCount < 6 and os.clock() - sayLastTime >= (delay/1000) then
+        Self.PrivateMessage(player, text, 0)
+        sayMessageCount = sayMessageCount + 1
+        sayLastTime = os.clock()
+        if sayMessageCount == 1 then
+            sayStartTime = os.clock()
+        end
+        return true        
+    end
+    return false    
+end 
 
 Module.New("PM With Players On Screen", function ()
     local names, amount = "", 0
@@ -31,7 +50,9 @@ Module.New("PM With Players On Screen", function ()
             end
         end
         if names ~= "" then
-            Self.PrivateMessage(RECEIVER, "On screen (" .. amount .. ") " .. names, 3000)
+            for i = 1, #RECEIVERS do
+                pm(RECEIVERS[i], "On screen (" .. amount .. ") " .. names, 0)
+            end    
             speakTime = os.clock()   
         end
     end        
