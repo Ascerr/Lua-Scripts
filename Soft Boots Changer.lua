0e3c46811e8d342d40e2d157b102906ec6e89529
Ascerr <rafalchabros1@gmail.com>
2022-10-19 20:15:24 +0200

    Update Soft Boots Changer.lua

--- Soft Boots Changer.lua
+++ Soft Boots Changer.lua
@@ -1,11 +1,13 @@
 --[[
     Script Name: 		Soft Boots Changer
-    Description: 		Load a new softs boots from backpacks if other worn.
+    Description: 		Load a new softs boots from backpacks if other worn or use equipment slot to auto charge.
     Author: 			Ascer - example
 ]]
 
-local SOFT_BOOTS_NEW_ID = 6529          -- id of new soft boots 
-local SOFT_BOOTS_USING = 3549           -- id of already using soft boots on feet.
+local config = {
+    soft_boots = {new = 6529, using = 3549, worn = 6530},       -- id for soft boots: new - brand new in backpack, using - working in equipment, worn - run out of time boots.
+    useEquipmentSlotToCharge = false                            -- allow to use worn equipment boots to charge it.
+}
 
 -- DON'T EDIT BELOW THIS LINE
 
@@ -14,25 +16,34 @@ Module.New("Soft Boots Changer", function (mod)
 	-- load feet.
 	local feet = Self.Feet().id
 
-	-- when no boots on feet.
-    if feet <= 0 then
+	-- when feet contains worn soft boots
+    if feet == config.soft_boots.worn then
 
-    	-- equip brand new softs.
-    	Self.EquipItem(SLOT_FEET, SOFT_BOOTS_NEW_ID, 1)
+        -- if allow to use feet slot
+        if config.useEquipmentSlotToCharge then
 
-    else	
+            -- use equimpent feet slot.
+            Self.UseItemFromEquipment(SLOT_FEET)
 
-    	-- when on feet are different boots than already using softs.
-    	if feet ~= SOFT_BOOTS_USING then
+        else
+            
+            -- dequip boots to backpack
+            Self.DequipItem(SLOT_FEET)
 
-    		-- dequip boots to any opened container.
-    		Self.DequipItem(SLOT_FEET)
+        end    
 
-    	end	
+    -- when equipment contains new eq boots (GAME BUG)
+    elseif feet == config.soft_boots.new then    
+
+        -- dequip boots to backpack
+        Self.DequipItem(SLOT_FEET)
+
+    -- when no boots in eq    
+    elseif feet == 0 then    
         
-    end
+        -- equip soft boots
+        Self.EquipItem(SLOT_FEET, config.soft_boots.new, 1, math.random(500, 800))
 
-    mod:Delay(500, 1200) -- set a delay
-    
+    end    
+
 end)
-
