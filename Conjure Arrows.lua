7d105873df12e0a35dbe1d92a90fd6b1fce97af0
Ascerr <rafalchabros1@gmail.com>
2022-10-28 20:51:41 +0200

    Update Conjure Arrows.lua

--- Conjure Arrows.lua
+++ Conjure Arrows.lua
@@ -4,10 +4,23 @@
     Author:             Ascer - example
 ]]
 
-local SPELL = {name = "exevo con", mana = 40}		-- name -> spell to cast, mana -> min mana to cast spell
+local SPELL = {name = "exevo con", mana = 400}		-- name -> spell to cast, mana -> min mana to cast spell
 local ITEM = {id = 3447, amount = 100, cap = 50}	-- id -> id of arrows, amount -> create only when below this amount, cap -> dont create when cap below.
+local MONSTERS = {enabled = false, safe = {"rat", "snake"}}  -- don't create when monsters on sceen enabled - true false, safe list, enter names.
 
 -- DON'T EDIT BELOW THIS LINE
+
+MONSTERS.safe = table.lower(MONSTERS.safe)
+
+function isMob()
+	if not MONSTERS.enabled then return false end
+	for _, c in ipairs(Creature.iMonsters(7, false)) do
+		if not table.find(MONSTERS.safe, string.lower(c.name)) then
+			return true
+		end	 
+	end
+	return false	
+end	--> returns true or false if is monster on screen (safe list reading)
 
 Module.New("Conjure Arrows", function (mod)
 	
@@ -29,8 +42,13 @@ Module.New("Conjure Arrows", function (mod)
 				-- check mana.
 				if Self.Mana() >= SPELL.mana then
 
-					-- cast Spell.
-					Self.Say(SPELL.name)
+					-- when no mobs
+					if not isMob() then
+
+						-- cast Spell.
+						Self.Say(SPELL.name)
+
+					end	
 
 				end
 
@@ -41,6 +59,6 @@ Module.New("Conjure Arrows", function (mod)
 	end	
 
 	-- module daly
-	mod:Delay(1000, 1500)
+	mod:Delay(500, 800)
 
 end)
