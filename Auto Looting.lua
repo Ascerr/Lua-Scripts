55327ceb4bb536ff7816eb4564814c9a4367c2ef
Ascerr <rafalchabros1@gmail.com>
2022-10-30 08:49:13 +0100

    Update Auto Looting.lua

--- Auto Looting.lua
+++ Auto Looting.lua
@@ -7,15 +7,25 @@
 local LOOT = {
 
 	{index = 0, items = {3031, 3492, 7138,  3577}}, -- index of container to loot 0 = first open backpack, items = table with items id
-	{index = 1, items = {3446}}
+	--{index = 0, items = {3031, 2853, "*"}}				-- here is example how to pickup all items "*" from container CROPSE missing only this ids listed: Character will pickup all items without gold and bag.
 	-- add your next index here
 
 }
 
 local DELAY = {200, 500} -- delay between movements.
-
+local CROPSE = {"The", "Demonic", "Dead", "Slain", "Dissolved", "Remains", "Elemental", "Split"} -- names of dead cropses, add your if list no contains enough, top pickup from bag add it to list: "Bag"
+local OPEN_NEXT_CONT_IF_FULL = true	-- when container you pickup loot is full open next as this same index.
 
 -- DONT'T EDIT BELOW THIS LINE
+
+function getCropse(name)
+    for _, element in ipairs(table.lower(CROPSE)) do
+        if string.lower(name):find(element) then
+            return true
+        end
+    end
+    return false
+end --> check if cropse name match to CROPSE table.
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------
 --> Function:       sortItems()
@@ -43,22 +53,78 @@ function sortItems()
 				for k, lootCont in ipairs(LOOT) do
 
 					-- check if table contains loot items and index is different.
-					if table.find(lootCont.items, item.id) and (cont.index) ~= lootCont.index then
+					if (table.find(lootCont.items, item.id) or table.find(lootCont.items, "*")) and (cont.index) ~= lootCont.index then
 
-						-- get empty slot in loot bp.
-						local destCont = Container.getInfo(lootCont.index)
-						local toSlot = 0
+						-- set default allow pickup on true
+						local allow = true
 
-						-- when container is open
-						if table.count(destCont) > 0 then
+						-- when we picking all items
+						if table.find(lootCont.items, "*")  then
 
-							-- calculate first empty slot.
-							toSlot = destCont.size - (destCont.size - destCont.amount)
+							-- when loot contains items to from table then don't pickup it
+							if table.find(lootCont.items, item.id) then
 
-						end
+								-- set pickup to false
+								allow = false
 
-						-- move item to destination container.
-						return Container.MoveItemToContainer(cont.index, (j-1), lootCont.index, (toSlot -1), item.id, item.count, 0)
+							end	
+
+							-- load info about container.
+        					local contInfo = Container.getInfo(cont.index)
+
+       						-- check for container name if contains dead cropse words.
+        					if not getCropse(contInfo.name) then
+
+        						-- allow pickup
+        						allow = false
+
+        					end	
+
+        				end		
+
+        				-- when allow pickup
+        				if allow then
+
+							-- get empty slot in loot bp.
+							local destCont = Container.getInfo(lootCont.index)
+							local toSlot = 0
+
+							-- when container is open
+							if table.count(destCont) > 0 then
+
+								-- check if cont is full
+					            if destCont.size == destCont.amount then
+
+					                -- check for possible container to open.
+					                local contItems = Container.getItems(lootCont.index)
+
+					                -- set param for opening new cont
+					                local isNewCont = false
+
+					                -- in loop check items.
+					                for m, it in ipairs(contItems) do
+
+					                    -- when is container attr
+					                    if Item.hasAttribute(it.id, ITEM_FLAG_CONTAINER) then
+
+					                        -- open container in this same index with delay 1000.
+					                        return Container.UseItem(lootCont.index, (m - 1), it.id, false, 1000)
+
+					                    end
+
+					                end
+
+					            end
+
+								-- calculate first empty slot.
+								toSlot = destCont.size
+
+							end
+
+							-- move item to destination container.
+							return Container.MoveItemToContainer(cont.index, (j-1), lootCont.index, (toSlot -1), item.id, item.count, 0)
+
+						end	
 
 					end
 				
