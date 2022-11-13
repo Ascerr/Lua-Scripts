677ea7ec1abbadcb78a62ee867721ee5b187271e
Ascerr <rafalchabros1@gmail.com>
2022-11-13 18:08:39 +0100

    Update Switch weapon on Creature.lua

--- Switch weapon on Creature.lua
+++ Switch weapon on Creature.lua
@@ -4,10 +4,13 @@
     Author: 			Ascer - example
 ]]
 
+
 local config = {
-	first = 3286,						-- id of first default weapon (wear it also when no target) 
-	second = 3268,						-- id of second weapon we will wear if attacking monsters from list below.
-	monsters = {"rotworm", "spider"}	-- list of monsters to wear second weapon.
+	first = {weapon = 3273, shield = 3426},						-- id of first default weapon and shield
+	second = {weapon = 3277, shield = 0},						-- id of second weapon (if no shield weapon is two handed type shield = 0)
+	amount = 2,													-- minimal amount of monsters to wear second weapon
+	range = 3,													-- max distance between you and monster to wear second weapon
+	monsters = {"rat", "spider"}								-- list of monsters to wear second weapon.
 }
 
 
@@ -16,63 +19,54 @@ local config = {
 -- change name to lower names
 config.monsters = table.lower(config.monsters)
 
-Module.New("Switch weapon on Creature", function ()
-	
-	-- when connected
-	if Self.isConnected() then
 
-		-- load target id
-		local target = Self.TargetID()
-
-		-- when target is
-		if target > 0 then
-
-			-- load mob
-			local mob = Creature.getCreatures(target)
-
-			-- when is valid.
-			if table.count(mob) > 1 then
-
-				-- load weapon id.
-				local weapon = Self.Weapon()
-
-				-- check for creature name
-				if table.find(config.monsters, string.lower(mob.name)) then
-
-					-- check for weapon
-					if weapon.id ~= config.second then
-
-						-- equip second weapon
-						Self.EquipItem(SLOT_WEAPON, config.second, 1, 500)
-
-					end	
-
-				else	
-
-					-- check for weapon (default first)
-					if weapon.id ~= config.first then
-
-						-- equip second weapon
-						Self.EquipItem(SLOT_WEAPON, config.first, 1, 500)
-
-					end	
-
-				end	
-
-			end	
-
-		else	
-
-			-- check for weapon (default first)
-			if weapon.id ~= config.first then
-
-				-- equip second weapon
-				Self.EquipItem(SLOT_WEAPON, config.first, 1, 500)
-
-			end	
-
+function canWearSecondWeapon()
+	local count = 0
+	for _, c in ipairs(Creature.iMonsters(config.range)) do
+		if table.find(config.monsters, string.lower(c.name)) then
+			count = count + 1
+			if count >= config.amount then return true end
 		end	
+	end
+	return false	
+end	--> Check if can change weapon to second (enough monsters in range)
 
+function switchWeapon(switchWeapon, switchShield)
+	local weapon = Self.Weapon()
+	local shield = Self.Shield()
+	if weapon.id == switchWeapon then 
+		if switchShield > 0 then
+			if shield.id ~= switchShield then
+				Self.EquipItem(SLOT_SHIELD, switchShield, 1, 500)
+			end
+		else
+			if shield.id > 0 then
+				Self.DequipItem(SLOT_SHIELD)
+			end
+		end
+	else
+		if switchShield == 0 then
+			if shield.id > 0 then
+				Self.DequipItem(SLOT_SHIELD)
+			else	
+				if weapon.id ~= switchWeapon then
+					Self.EquipItem(SLOT_WEAPON, switchWeapon, 1, 500)
+				end	
+			end
+		else	
+			if weapon.id ~= switchWeapon then
+				Self.EquipItem(SLOT_WEAPON, switchWeapon, 1, 500)
+			end	
+		end			
 	end	
+end	--> Switch weapon and shield in slots
 
+Module.New("Switch weapon on Creature", function ()
+	if Self.isConnected() then
+		if canWearSecondWeapon() then
+			switchWeapon(config.second.weapon, config.second.shield)
+		else
+			switchWeapon(config.first.weapon, config.first.shield)
+		end
+	end	
 end)
