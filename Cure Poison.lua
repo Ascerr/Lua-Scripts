8b850eebc7d18be8c66385b4d7fba065e7e16560
Ascerr <rafalchabros1@gmail.com>
2022-11-30 20:23:18 +0100

    Update Cure Poison.lua

--- Cure Poison.lua
+++ Cure Poison.lua
@@ -4,8 +4,19 @@
     Author:             Ascer - example
 ]]
 
+local config = {
+	spell = "exana pox",		-- name of spell to cast
+	minHealth = 50,				-- don't cast if self hpperc below.
+	ifNoMonsters = false,		-- true/false when monsters don't cast
+}
+
 Module.New("Cure Poison", function ()
-    if Self.isPoisioned() then
-        Self.CastSpell("exana pox", 30)
-    end    
+    if Self.isConnected() then
+	    if Self.isPoisioned() then
+	    	if Self.HealthPercent() >= config.minHealth then    
+	        	if config.ifNoMonsters and table.count(Creature.iMonsters(7, false)) > 0 then return end
+	        	Self.CastSpell(config.spell, 30)
+	        end	
+	    end
+	end        
 end)
