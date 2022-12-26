b09e5a7e81cb1e1f2a204202b78824b3607a133a
Ascerr <rafalchabros1@gmail.com>
2022-12-26 18:10:34 +0100

    Update Multifloor Player Logout.lua

--- Multifloor Player Logout.lua
+++ Multifloor Player Logout.lua
@@ -5,15 +5,23 @@
 ]]
 
 local FRIENDS = {"friend1", "friend2"} 		-- list of friends
+local DONT_LOGOUT_WHEN_GM_APPEAR = {enabled = true, keywords = {"GM ", "CM ", "Admin ", "ADM ", "Gamemaster"}}
 local BELOW = true							-- check for players below you
 local ABOVE = false							-- check for player above you
 local LEVELS = 2							-- search for one floor above or below / limit is 2 / do not check floors below on level 7
-local RELOGIN = {enabled = false, delay = 6} -- relogin to game when player detected or lost connection. enabled = true/false, delay = 6 min or immediately when lost connection
+local RELOGIN = {enabled = true, delay = 0.1} -- relogin to game when player detected or lost connection. enabled = true/false, delay = 6 min or immediately when lost connection
 
 -- DON'T EDIT BELOW THIS LINE
 
 FRIENDS = table.lower(FRIENDS) -- convert table to lower strings.
 logoutTime, printfTime, LOGOUT_TIME, LAST_PLAYER = 0, 0, "", ""
+
+function isGM(name)
+	for j = 1, #DONT_LOGOUT_WHEN_GM_APPEAR.keywords do
+        if string.instr(name, DONT_LOGOUT_WHEN_GM_APPEAR.keywords[j]) then return true end
+    end
+    return false     	
+end	--> check is name contains GM keywords.
 
 Module.New("Multifloor Player Logout", function (mod)
 	if Self.isConnected() then
@@ -21,7 +29,7 @@ Module.New("Multifloor Player Logout", function (mod)
 		logoutTime = 0
 		local selfpos = Self.Position()
 		for i, player in pairs(Creature.iPlayers(7, true)) do  -- get only above and below
-			if not table.find(FRIENDS, string.lower(player.name)) then
+			if not table.find(FRIENDS, string.lower(player.name)) and (not DONT_LOGOUT_WHEN_GM_APPEAR.enabled or (DONT_LOGOUT_WHEN_GM_APPEAR.enabled and not isGM(player.name))) then
 				local var = (BELOW and (selfpos.z - player.z) <= 0 and (selfpos.z - player.z) >= (-LEVELS)) or (ABOVE and (selfpos.z - player.z) >= 0 and (selfpos.z - player.z) <= LEVELS)
 				if var then
 					Self.Logout()
