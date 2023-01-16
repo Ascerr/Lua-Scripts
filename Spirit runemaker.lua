0ef76bd360509b9fa2da0e7163f473bf26870db1
Ascerr <rafalchabros1@gmail.com>
2023-01-16 09:31:26 +0100

    Update Spirit runemaker.lua

--- Spirit runemaker.lua
+++ Spirit runemaker.lua
@@ -7,13 +7,23 @@
 
 local config = {
 	spell = "adori",				-- spell to cast 
-	spiritPercent = 90				-- when spirit percent is equal or above this value character will make rune.
+	spiritPercent = 90,				-- when spirit percent is equal or above this value character will make rune.
+	randomize = {-5, 5},			-- randomize spirit percent while creating runes, ~5%
+	castTimes = 1,					-- cast spell once or more.
 }
+
+-- DON'T EDIT BELOW THIS LINE
+
+local randPerc = config.spiritPercent + math.random(config.randomize[1], config.randomize[2])
 
 Module.New("Spirit runemaker", function(mod)
 	if Self.isConnected() then
-		if Self.SpiritPercent() >= config.spiritPercent then
-			Self.Say(config.spell)
+		if Self.SpiritPercent() >= randPerc then
+			for i = 1, config.castTimes do
+				Self.Say(config.spell)
+				wait(2200, 3000)
+			end
+			randPerc = config.spiritPercent + math.random(config.randomize[1], config.randomize[2])
 		end	
 	end	
 	mod:Delay(500, 1000)
