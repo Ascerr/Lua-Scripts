9291c531a4b6f55ba2b9a12be8d7bd70d7d158fc
Ascerr <rafalchabros1@gmail.com>
2023-01-27 11:30:53 +0100

    Update Use Training Wand with Dummy after login.lua

--- Use Training Wand with Dummy after login.lua
+++ Use Training Wand with Dummy after login.lua
@@ -1,12 +1,12 @@
 --[[
     Script Name: 		Use Training Wand with Dummy after login
-    Description: 		When you lost connection, character will use training wand with dummy (wand must be in container)
+    Description: 		When you lost connection, character will use training wand with dummy (wand must be on ground)
     Author: 			Ascer - example
 ]]
 
 local config = {
-	wand = 6014,								-- training wand id.
-	dummy = {x = 32359, y = 32235, z = 6}		-- position of training dummy
+	wand = {x = 32358, y = 32234, z = 6},	-- training wand position on ground.
+	dummy = {x = 32359, y = 32235, z = 6}	-- position of training dummy
 }
 
 -- DON'T EDIT BELOW THIS LINE
@@ -15,7 +15,7 @@ Module.New("Use Training Wand with Dummy after login", function ()
 	if Self.isConnected() then
 		if useWand and Container.Amount() > 0 then
 			wait(1000)
-			Self.UseItemWithGround(config.wand, config.dummy.x, config.dummy.y, config.dummy.z, 0)
+			Map.UseItemWithGround(config.wand.x, config.wand.y, config.wand.z, config.dummy.x, config.dummy.y, config.dummy.z, 0)
 			useWand = false
 		end	
 	else
