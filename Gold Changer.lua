4a5a149a5d2e9ce6f5837f8942ca3a9b441bfb1a
Ascerr <rafalchabros1@gmail.com>
2023-02-20 14:42:56 +0100

    Update Gold Changer.lua

--- Gold Changer.lua
+++ Gold Changer.lua
@@ -8,6 +8,7 @@ local GOLD_ID = 3031											-- id of gold coin
 local PLATINUM_ID = 3035										-- id of platinum coin
 local USE_GOLD_CONVERTER_ITEM = {enabled = false, item = 7889, mode = "use"}	-- on some servers there is an item called Gold Converter, @enabled - true/false, @item - ID of gold converter, @mode - type of use: "with" - use item with gold, "use" - just use gold converter like food. 
 local ONLY_IF_NO_MONSTER_ON_SCREEN = false						-- true/false when any monsters on screen don't convert gold to avoid exhausting.
+local COLLECT_GOLD_TO_POUCH = false								-- use gold no matter of amount to collect it into gold pouch.
 
 -- DON'T EDIT BELOW
 
@@ -23,7 +24,7 @@ Module.New("Gold Changer", function()
 		local contItems = cont.items
 		for j = 1, #contItems do
 			local item = contItems[j]
-			if item.count == 100 then
+			if item.count == 100 or COLLECT_GOLD_TO_POUCH then
 				if item.id == GOLD_ID or item.id == PLATINUM_ID then
 					if USE_GOLD_CONVERTER_ITEM.enabled then
 						local converter = Container.FindItem(USE_GOLD_CONVERTER_ITEM.item)
