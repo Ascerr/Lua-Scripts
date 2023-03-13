44d7bc26bfa95319ac2c4773dd39c41824955401
Ascerr <rafalchabros1@gmail.com>
2023-03-13 14:15:40 +0100

    Update Heal with eating food.lua

--- Heal with eating food.lua
+++ Heal with eating food.lua
@@ -1,13 +1,13 @@
 --[[
     Script Name:        Heal with eating food
-    Description:        On some servers eating food give extra health so lets use it.
+    Description:        On some servers eating food give extra health and mana so lets use it.
     Author:             Ascer - example
 ]]
 
 local config = {
-	hpperc = 80,							-- when self health percent below this value eat food.
-	food = {35348, 36090, 36004},			-- possible foods to eat
-	delay = 3000							-- eating delay
+	health = {perc = 80, food = {35348, 36090, 36004}},		-- heal health, percent, possible food
+	mana = {perc = 50, food = {123}},						-- heal mana, precent, possible food	
+	delay = 3000											-- eating delay
 }
 
 -- DON'T EDIT BELOW
@@ -16,14 +16,23 @@ local useTime = 0
 -- mod 200ms
 Module.New("Heal with eating food", function()
 	if Self.isConnected() then
-		if Self.HealthPercent() <= config.hpperc then
+		if Self.HealthPercent() <= config.health.perc then
 			if os.clock() - useTime >= config.delay/1000 then
-				local item = Container.FindItem(config.food)
+				local item = Container.FindItem(config.health.food)
 				if table.count(item) > 0 then
 					Container.UseItem(item.index, item.slot, item.id, false, 0)
 					useTime = os.clock()
 				end
 			end		
-		end	
+		end
+		if Self.ManaPercent() <= config.mana.perc then
+			if os.clock() - useTime >= config.delay/1000 then
+				local item = Container.FindItem(config.mana.food)
+				if table.count(item) > 0 then
+					Container.UseItem(item.index, item.slot, item.id, false, 0)
+					useTime = os.clock()
+				end
+			end		
+		end		
 	end	
 end)
