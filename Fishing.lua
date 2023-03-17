3e9f2e7824f3d25182e05657ed0a0de587dc8160
Ascerr <rafalchabros1@gmail.com>
2023-03-17 08:31:14 +0100

    Update Fishing.lua

--- Fishing.lua
+++ Fishing.lua
@@ -4,10 +4,11 @@
     Author: 			Ascer - example
 ]]
 
-local MAIN_DELAY = {1000, 1500}  -- miliseconds use fishing rod time
-local FISHING_ROD = 3483		 -- fishing rod id
-local WATER_TILES =  {617, 618, 619, 620, 621} --  {4597,4598,4599,4600, 4601, 4602, 4609}  -- ids of water fields
-local MIN_CAP = -1		-- minimal capity to fishing.
+local MAIN_DELAY = {1000, 1500}  				-- miliseconds use fishing rod time
+local FISHING_ROD = 3483		 				-- fishing rod id
+local WATER_TILES =  {617, 618, 619, 620, 621, 4597,4598,4599,4600, 4601, 4602, 4609}  -- ids of water fields
+local MIN_CAP = -1								-- minimal capity to fishing.
+local FISHING_WITH_USE_WATER = false			-- true/false - on some servers fishing is through usage of water. e.g. Souls of Elysium
 
 -- DONT'T EDIT BELOW THIS LINE 
 
@@ -26,11 +27,16 @@ Module.New("Fishing", function (mod)
 		
 		if load then -- load a new tiles
 			fishingPos = {} -- reset fishing pos
-			for x = -7, 7 do
-				for y = -5, 5 do
+			local osx, osy = 7, 5
+			if FISHING_WITH_USE_WATER then
+				osx = 1
+				osy = 1
+			end	
+			for x = -osx, osx do
+				for y = -osy, osy do
 					local map = Map.GetTopMoveItem(pos.x + x, pos.y + y, pos.z)
 					if table.find(WATER_TILES, map.id) then	
-						table.insert(fishingPos, {x = pos.x + x, y = pos.y + y, z = pos.z})
+						table.insert(fishingPos, {x = pos.x + x, y = pos.y + y, z = pos.z, id = map.id})
 					end		
 				end
 			end
@@ -39,7 +45,11 @@ Module.New("Fishing", function (mod)
 		-- set random tile to fish.
 		if table.count(fishingPos) > 0 then
 			local tile = fishingPos[math.random(1, table.count(fishingPos))] 
-			Self.UseItemWithGround(FISHING_ROD, tile.x, tile.y, tile.z, 100) -- use fishing rod.
+			if FISHING_WITH_USE_WATER then
+				Map.UseItem(tile.x, tile.y, tile.z, tile.id, 0, 100)				-- use water tile.
+			else	
+				Self.UseItemWithGround(FISHING_ROD, tile.x, tile.y, tile.z, 100) 	-- use fishing rod.
+			end	
 		end	
 
 	end	
