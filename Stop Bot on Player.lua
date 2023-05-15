b8a1062cdd1ea71bf88820560536dab4806da8c9
Ascerr <rafalchabros1@gmail.com>
2023-05-15 10:12:01 +0200

    Update Stop Bot on Player.lua

--- Stop Bot on Player.lua
+++ Stop Bot on Player.lua
@@ -4,7 +4,8 @@
     Author: 			Ascer - example
 ]]
 
-local FRIENDS = {"friend1", "friend2"}
+local FRIENDS = {"friend1", "friend2"}      -- friends nicks to avoid
+local PAUSE_LUA_SCRIPTS = false             -- disable lua scripts too !WARRING script don't restore bot again, you will need to manually press Ctrl+P
 
 -- DONT'T EDIT BELOW THIS LINE 
 
@@ -15,7 +16,7 @@ Module.New("Stop Bot on Player", function ()
     for i = 1, #players do
         local player = players[i]
         if not table.find(list, string.lower(player.name)) then
-            return Rifbot.setEnabled(false) -- disable bot when player on screen.
+            return Rifbot.setEnabled(false, PAUSE_LUA_SCRIPTS) -- disable player when player on screen.
         end
     end
     Rifbot.setEnabled(true) -- enable bot not players found
