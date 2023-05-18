7a98f5d05076f312a881515c64fef84485e3af67
Ascerr <rafalchabros1@gmail.com>
2023-05-18 17:48:06 +0200

    Update and rename Player On Screen Alarm.lua to Player On Screen + Skull Alarm.lua

--- Player On Screen + Skull Alarm.lua
+++ Player On Screen + Skull Alarm.lua
@@ -1,21 +1,22 @@
 --[[
-    Script Name: 		Player On Screen Alarm
-    Description: 		Play sound player is on screen. (configurable safe list and time required to player stay on screen) 
+    Script Name: 		Player On Screen + Skull Alarm
+    Description: 		Play sound player is on screen. (configurable safe list, time required to player stay on screen, check only for skulls) 
     Author: 			Ascer - example
 ]]
 
 local SAFE_LIST = {"Friend1", "Friend2"}  -- add here your training friend or defense monster for example. Names need to by with Capital letter.
 local MINIMAL_TIME_STAY_ON_SCREEN = 0     -- minimal amount of miliseconds (1000ms = 1s) player stay on screen to alert. For 5s put 5000 
+local CHECK_ONLY_FOR_SKULLS = false       -- true/false check only for players that have skull red or white
 
 -- DON'T EDIT BELOW THIS LINE
 
 local enterTime = 0
 
 -- loop function
-Module.New("Player On Screen Alarm", function ()
+Module.New("Player On Screen + Skull Alarm", function ()
     local isPlayer = false
     for i, player in pairs(Creature.iPlayers(7)) do
-        if not table.find(SAFE_LIST, player.name) then
+        if not table.find(SAFE_LIST, player.name) and (not CHECK_ONLY_FOR_SKULLS or (CHECK_ONLY_FOR_SKULLS and player.skull >= SKULL_WHITE)) then
             isPlayer = true
             if enterTime <= 0 then
                 enterTime = os.clock()
