c864991a38fb200e8e186421220be9fe7bda9def
Ascerr <rafalchabros1@gmail.com>
2023-05-22 14:43:56 +0200

    Update Items Drop from dead bodies.lua

--- Items Drop from dead bodies.lua
+++ Items Drop from dead bodies.lua
@@ -1,19 +1,42 @@
 --[[
     Script Name:        Drop Items
-    Description:        Drop items under your character from any open container
+    Description:        Drop items under your character from specific containers names depend on cap.
     Author:             Ascer - example
 ]]
 
-local ITEMS = {3031, 3492}			-- items to drop
-local DROP_DELAY = {200, 300} 		-- time between actions
+local config = {
+	ids = {3031, 3607},	-- ids of items to drop
+	cropse = {"The", "Demonic", "Dead", "Slain", "Dissolved", "Remains", "Elemental", "Split"}, -- names od dead cropses, add your if list no contains enough
+	whenCapBelow = 50
+}
 
 -- DON'T EDIT BELOW THIS LINE
 
-Module.New("Drop Items", function ()
-	local item = Container.FindItem(ITEMS)
-	if item ~= false then
-		local pos = Self.Position()
-		Container.MoveItemToGround(item.index, item.slot, pos.x, pos.y, pos.z, item.id, item.count, 0)
-	end
-	mod:Delay(DROP_DELAY[1], DROP_DELAY[2])	
+function getCropse(name)
+    for _, element in ipairs(table.lower(config.cropse)) do
+        if string.lower(name):find(element) then
+            return true
+        end
+    end
+    return false
+end --> check if cropse name is valid
+
+Module.New("Drink fluids from dead monsters", function()
+	if Self.isConnected() then
+   		if Self.Capity() <= config.whenCapBelow then
+	   		local containers = Container.getItems()
+			for i, container in ipairs(containers) do
+	    		local contInfo = Container.getInfo(container.index)
+	    		if getCropse(contInfo.name) then
+	        		local items = container.items
+	        		for j, item in ipairs(items) do
+						if table.find(config.ids, item.id) then
+							local pos = Self.Position()
+							return Container.MoveItemToGround(container.index, (j-1), pos.x, pos.y, pos.z, item.id, item.count, 0)	
+						end	
+					end       
+	            end
+	        end
+	    end    
+	end	
 end)
