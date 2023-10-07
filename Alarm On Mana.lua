88ee6fe44964f04891ff9a27f02410a9dc28e97c
Ascerr <rafalchabros1@gmail.com>
2023-10-07 11:21:37 +0200

    Update Alarm On Mana.lua

--- Alarm On Mana.lua
+++ Alarm On Mana.lua
@@ -1,14 +1,40 @@
 --[[
     Script Name: 		Alarm On Mana
-    Description: 		Play sound if your mana points are below MANA value.
+    Description: 		Play sound if your mana points are below or above MANA value.
     Author: 			Ascer - example
 ]]
 
-local MANA = 1000				-- when mana will equal or above this value play sound.
-local ALARM_NAME = "Low Mana.mp3" -- find more in Rifbot Lib.lua
+local config = {
+	mana = 150,				-- mana points value to active alarm if it will below or equal or above.
+	typeOf = "below",		-- type of mana to check possible are: "below", "equal", "above", "below-equal", "above-equal"
+	pauseBot = false		-- true/false pause bot with lua scripts.
+}
 
+-- DON'T EDIT BELOW THIS LINE
+config.typeOf = string.lower(config.typeOf)
+
+-- module run function inside loop
 Module.New("Alarm On Mana", function ()
-	if Self.Mana() >= MANA then
-		Rifbot.PlaySound(ALARM_NAME)
-	end	
+	if Self.isConnected() then
+		local mp = Self.Mana()
+		local cond = false
+		if config.typeOf == "below" then
+			cond = mp < config.mana
+		elseif config.typeOf == "below-equal" then
+			cond = mp <= config.mana	
+		elseif config.typeOf == "equal" then
+			cond = mp == config.mana
+		elseif config.typeOf == "above" then
+			cond = mp > config.mana
+		elseif config.typeOf == "above-equal" then
+			cond = mp >= config.mana			
+		end
+		if cond then
+			Rifbot.PlaySound()
+			if config.pauseBot then
+				Rifbot.setEnabled(false, true)
+			end	
+			print("Detected mana " .. config.typeOf .. " " .. config.mana .. " points.")
+		end	
+	end		
 end)
