ccbce5bb61d1211315517a2def4113c11270dcf1
Ascerr <rafalchabros1@gmail.com>
2024-01-10 08:22:15 +0100

    Update Heal with eating food.lua

--- Heal with eating food.lua
+++ Heal with eating food.lua
@@ -7,17 +7,17 @@
 local config = {
 	health = {perc = 80, food = {35348, 36090, 36004}},		-- heal health, percent, possible food
 	mana = {perc = 50, food = {123}},						-- heal mana, precent, possible food	
-	delay = 3000											-- eating delay
+	moduleDelay = {2000, 4000}								-- execute module (script loop) in random times between this delay.
 }
 
 -- DON'T EDIT BELOW
 local useTime = 0
 
 -- mod 200ms
-Module.New("Heal with eating food", function()
+Module.New("Heal with eating food", function(mod)
 	if Self.isConnected() then
 		if Self.HealthPercent() <= config.health.perc then
-			if os.clock() - useTime >= config.delay/1000 then
+			if os.clock() - useTime >= 1 then
 				local item = Container.FindItem(config.health.food)
 				if table.count(item) > 0 then
 					Container.UseItem(item.index, item.slot, item.id, false, 0)
@@ -26,7 +26,7 @@ Module.New("Heal with eating food", function()
 			end		
 		end
 		if Self.ManaPercent() <= config.mana.perc then
-			if os.clock() - useTime >= config.delay/1000 then
+			if os.clock() - useTime >= 1 then
 				local item = Container.FindItem(config.mana.food)
 				if table.count(item) > 0 then
 					Container.UseItem(item.index, item.slot, item.id, false, 0)
@@ -34,5 +34,6 @@ Module.New("Heal with eating food", function()
 				end
 			end		
 		end		
-	end	
+	end
+	mod:Delay(config.moduleDelay[1], config.moduleDelay[2])	
 end)
