f2ba0b808df1e50f08b865d520d20507565df1a1
Ascerr <rafalchabros1@gmail.com>
2024-01-26 14:14:14 +0100

    Update Switch Ering.lua

--- Switch Ering.lua
+++ Switch Ering.lua
@@ -4,19 +4,35 @@
     Author: 			Ascer - example
 ]]
 
-local ENERGY_RING = {on = 3088, off = 3051} -- set id for ering on - equiped, off - not
-local HPPERC = 75							-- when hpperc below or equal equip ring else dequip
+local ENERGY_RING = {on = 3088, off = 3051}     -- set id for ering on - equiped, off - not
+local HPPERC = 75							    -- when hpperc below or equal rquip ring else dequip
+local ONLY_WHEN_NO_PLAYERS_ON_SCREEN = false    -- true/false will ering when low hp but also when no players on screen.
 
+
+function getPlayers()
+    for _, player in ipairs(Creature.iPlayers(7, false)) do
+        return 1
+    end
+    return 0    
+end    
+ 
 Module.New("Switch Ering", function (mod)
     local hp = Self.HealthPercent()
     local ring = Self.Ring()
     if hp <= HPPERC then
+        if ONLY_WHEN_NO_PLAYERS_ON_SCREEN then
+            local players = getPlayers()
+        else
+            local players = 0
+        end
+        if players > 0 then return end     
         if ring.id ~= ENERGY_RING.on then
             Self.EquipItem(SLOT_RING, ENERGY_RING.off, 1)
         end
     else
         if ring.id == ENERGY_RING.on then
-            Self.DequipItem(SLOT_RING) -- dequip ring
+            Self.DequipItem(SLOT_RING, SLOT_AMMO, 1) -- dequip ring
+            --Self.EquipItem(SLOT_RING, 3052, 1)
         end
     end
     mod:Delay(200, 700)
