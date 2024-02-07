da4ae8f6cd4e9dec8f99f3a0866f21b69fc85ad3
Ascerr <rafalchabros1@gmail.com>
2024-02-07 19:55:13 +0100

    Update Ban Spectators on Cast.lua

--- Ban Spectators on Cast.lua
+++ Ban Spectators on Cast.lua
@@ -2,9 +2,13 @@
     Script Name:        Ban Spectators on Cast
 
     Description:        Auto ban new spectators joined to your cast. 
-    Required:			Valid message about joined specator inside line 11.              
+    Required:			Valid message about joined specator inside line 16 or 29.              
     Author:             Ascer - example 
 ]]
+
+local USE_PROXY_TEXT = false	-- true/false use different method to catching signals example on Kasteria
+
+if not USE_PROXY_TEXT then
 
 function proxy(messages) 
 	for i, msg in ipairs(messages) do 
@@ -17,3 +21,17 @@ function proxy(messages)
 	end 
 end 
 Proxy.New("proxy")
+
+else
+
+function proxy(messages) 
+	for i, msg in ipairs(messages) do 
+		local spectator = string.match(msg.message, "(.+) joins your stream.")
+		if spectator ~= nil then
+            Self.Say("!cast ban " .. spectator)
+        end   
+	end 
+end 
+Proxy.TextNew("proxy")
+
+end
