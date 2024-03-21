d2bed86415bea0807df6f730edbcbf9efeef2f0e
Ascerr <rafalchabros1@gmail.com>
2024-03-21 09:55:08 +0100

    Update Cast Spell On Time.lua

--- Cast Spell On Time.lua
+++ Cast Spell On Time.lua
@@ -4,12 +4,12 @@
     Author:             Ascer - example
 ]]
 
-local SPELL = "utevo lux"           -- spell we will cast in game
-local EVERY_TIME_SECONDS = 80       -- every this time in seconds will countinue cast spell
-local ADD_RANDOM = false            -- add random time +5, 10 seconds [true/false]
-local IF_PLAYER_DONT_CAST = true    -- don't cast if on screen, ignore players from Friends.txt
-local WEAR_BLANK_RUNE = false       -- wear blank rune before casting spell
-local BLANK_RUNE_ID = 2260          -- id of blank rune
+local SPELL = "utevo lux"                               -- spell we will cast in game
+local EVERY_TIME_SECONDS = 80                           -- every this time in seconds will countinue cast spell
+local ADD_RANDOM = {enabled = false, time = {5, 10}}    -- add random time +5, 10 seconds [true/false]
+local IF_PLAYER_DONT_CAST = false                       -- don't cast if on screen, ignore players from Friends.txt
+local WEAR_BLANK_RUNE = false                           -- wear blank rune before casting spell
+local BLANK_RUNE_ID = 2260                               -- id of blank rune
 
 -- DONT'T EDIT BELOW THIS LINE
 
@@ -63,10 +63,10 @@ Module.New("Cast Spell On Time", function ()
                 mainTime = os.clock()
 
                 -- if we adding extra random time
-                if ADD_RANDOM then
+                if ADD_RANDOM.enabled then
 
                     -- set random time
-                    randomTime = math.random(5, 10)
+                    randomTime = math.random(ADD_RANDOM.time[1], ADD_RANDOM.time[2])
 
                 end
 
