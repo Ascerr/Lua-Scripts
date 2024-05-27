15cb7e7ee37db15d7d5e3b3fcebdbd187e570167
Ascerr <rafalchabros1@gmail.com>
2024-05-27 08:14:05 +0200

    Update Switch SSA.lua

--- Switch SSA.lua
+++ Switch SSA.lua
@@ -1,16 +1,27 @@
 --[[
     Script Name: 		Switch SSA
-    Description: 		Equip SSA when mana is below 40% and dequip if above.
+    Description: 		Equip SSA when mana or hp is below 40% and dequip if above.
     Author: 			Ascer - example
 ]]
 
 local AMULET_ID = 3081 		-- id amulet to wear (default ssa 3081)
-local MPPERC = 75			-- when mana percent <= this percent then wear ssa else dequip.
+local TYPE = "mana"         -- which value to read "mana" or "hp"
+local VALUE = 75			-- when mpperc or hpperc <= this percent then wear ssa else dequip.
+
+
+-- DON'T EDIT BELOW THIS LINE
+
+TYPE = string.lower(TYPE)
 
 Module.New("Switch SSA", function (mod)
-    local mp = Self.ManaPercent()
+    local val = 0
+    if TYPE == "mana" then
+        val = Self.ManaPercent()
+    else
+        val = Self.HealthPercent()
+    end 
     local amulet = Self.Amulet()
-    if mp <= MPPERC then
+    if val <= VALUE then
         if amulet.id ~= AMULET_ID then
             Self.EquipItem(SLOT_AMULET, AMULET_ID, 1)
         end
