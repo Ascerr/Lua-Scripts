2722882390c378d81cdaabe53a73d2df40bc0bdd
Ascerr <rafalchabros1@gmail.com>
2024-07-13 10:19:32 +0200

    Update Runemax & SpellMax.lua

--- Runemax & SpellMax.lua
+++ Runemax & SpellMax.lua
@@ -8,6 +8,7 @@ local CONFIG = {
     rune = {enabled = true, id = 3174, range = 7, hpperc = {min = 0, max = 100}},              -- shoot rune with target, [id] of rune (3155 is SD), [range] - sqms to target, [enabled] - true/false
     spell = {enabled = true, name = "exori con", range = 7, hpperc = {min = 0, max = 100}},   -- cast spell with target, [name] of spell (exori con), [range] - cast if monster in distance to you , [hpperc] - cast shoot only when monster hpperc will between walue [min] and [max]
     health = 60,                                                -- dont shoot/cast if self character hpperc below this value
+    amount = 1,                                                 -- min monsters amount
     monsters = {"Rat", "Rotworm"}                                 -- monsters names to cast spell or shoot rune.    
 }
 
@@ -15,6 +16,16 @@ local CONFIG = {
 
 -- converting table to lowercase
 CONFIG.monsters = table.lower(CONFIG.monsters)
+
+function getMonsters()
+    local count = 0
+    for _, c in ipairs(Creature.iMonsters(7, false)) do
+        if table.find(CONFIG.monsters, string.lower(c.name)) then
+            count = count + 1
+        end    
+    end
+    return count    
+end --> return amount of monsters on screen.
 
 Module.New("Runemax & SpellMax", function()
 
@@ -34,7 +45,7 @@ Module.New("Runemax & SpellMax", function()
             local c = Creature.getCreatures(target)
 
             -- when is table.
-            if table.count(c) > 0 then
+            if table.count(c) > 0 and getMonsters() > CONFIG.amount then
 
                 -- when creature is alive and is valid monster name.
                 if c.hpperc > 0 and table.find(CONFIG.monsters, string.lower(c.name)) then
