0b8f6c90087c293b389c2f659c8dcc0f02dd03ac
Ascerr <rafalchabros1@gmail.com>
2025-03-24 07:53:39 +0100

    Update [on label] Withdraw mana fluids from ground.lua

--- [on label] Withdraw mana fluids from ground.lua
+++ [on label] Withdraw mana fluids from ground.lua
@@ -95,7 +95,7 @@ function withdrawMF()
                                     if item.count < toPcikup then
                                         toPcikup = item.count
                                     end    
-                                    Container.MoveItemToContainer(index, i - 1, config.container_index, dest.amount, item.id, 1, config.moveItemsDelay)
+                                    Container.MoveItemToContainer(index, i - 1, config.container_index, dest.amount, item.id, item.count, config.moveItemsDelay)
                                     return false
                                 end    
                             else
