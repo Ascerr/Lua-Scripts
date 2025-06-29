c4dee3fe46e07d49f50ae5c03cadbfc5e3ba3acf
Ascerr <rafalchabros1@gmail.com>
2025-06-29 12:09:13 +0200

    Update Switch SSA.lua

--- Switch SSA.lua
+++ Switch SSA.lua
@@ -4,10 +4,10 @@
     Author: 			Ascer - example
 ]]
 
-local AMULET_ID = 3081 		-- id amulet to wear (default ssa 3081)
-local TYPE = "mana"         -- which value to read "mana" or "hp"
-local VALUE = 75			-- when mpperc or hpperc <= this percent then wear ssa else dequip.
-
+local AMULET_ID = 3081 		     -- id amulet to wear (default ssa 3081)
+local TYPE = "mana"              -- which value to read "mana" or "hp"
+local VALUE = 75			     -- when mpperc or hpperc <= this percent then wear ssa else dequip.
+local ONLY_IF_PLAYER = true     -- true/false wear amulet only if player on screen
 
 -- DON'T EDIT BELOW THIS LINE
 
@@ -21,7 +21,8 @@ Module.New("Switch SSA", function (mod)
         val = Self.HealthPercent()
     end 
     local amulet = Self.Amulet()
-    if val <= VALUE then
+    local var = ((not ONLY_IF_PLAYER) or (ONLY_IF_PLAYER and table.count(Creature.iPlayers(7, false)) > 0)) 
+    if val <= VALUE and var then
         if amulet.id ~= AMULET_ID then
             Self.EquipItem(SLOT_AMULET, AMULET_ID, 1)
         end
