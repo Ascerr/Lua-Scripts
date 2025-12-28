d95ece3af4e52dc168f4f8ec31223ff27819a0f0
Ascerr <rafalchabros1@gmail.com>
2025-12-28 17:39:32 +0100

    Add files via upload

---
+++ Pickup Spears (old map).lua
@@ -0,0 +1,47 @@
+
+local SPEARS = {3277}     			-- Spears ids to pickup
+local DELAY = {500, 1000}			-- delay for pickup object
+local PICKUP_AFTER_KILL = false		-- pick only when last attacked monster died or when no more spear in EQ.
+local RANGE = 1						-- search sqms around
+
+-- DON'T EDIT BELOW
+
+local lastT = 0
+
+function rememberTarget()
+	local t = Self.TargetID()
+	if t > 0 and lastT == 0 then
+		lastT = t
+	end	
+end	--> Store last target id
+
+function targetDied()
+	if lastT > 0 then
+		local c = Creature.getCreatures(lastT)
+		if table.count(c) > 0 then return false end
+		return true 
+	end	
+end	--> check if last target died (exisits or not)
+
+
+Module.New("Pickup Spears", function (mod)
+	if Self.isConnected() then
+		rememberTarget() -- remember last target
+		local var = (not PICKUP_AFTER_KILL) or (PICKUP_AFTER_KILL and (targetDied() or (not table.find(SPEARS, Self.Weapon().id) and not table.find(SPEARS, Self.Shield().id)) ))
+		if var then
+			local map = Map.getArea(RANGE) -- load map with 1 sqm range
+			for i, square in pairs(map) do
+				local sqareItems = square.items
+				for j, item in pairs(sqareItems) do
+					if table.find(SPEARS, item.id) then
+						Self.EquipItemFromGround(SLOT_WEAPON, square.x, square.y, square.z, item.id, item.count, 0)
+						break
+					end
+				end			
+			end
+			lastT = Self.TargetID()
+		end	
+	end
+	mod:Delay(DELAY[1], DELAY[2])		
+end) 
+
